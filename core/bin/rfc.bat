b9465af42573b3330ae3980f23cd78fe79ef40e7
Lenik <pub.lenik@bodz.net>
2004-10-08 08:08:30 +0000

    [index] Add rfc to search rfc docs and open them.

---
+++ core/bin/rfc.bat
@@ -0,0 +1,44 @@
+@echo off
+
+	set rfc_home=L:\files\refer\rfc
+	set rfc_edit=notepad
+
+	if "%1"=="u" (
+		set rfc_edit=uedit32
+		shift
+	)
+
+	if "%1"=="" goto help
+
+:start
+	pushd %rfc_home% >nul
+
+	if exist "rfc%1.*" (
+		for %%i in (rfc%1.*) do %rfc_edit% "%%i"
+		goto end
+	)
+
+	if exist "rfc%1 *.*" (
+		for %%i in ("rfc%1 *.*") do %rfc_edit% "%%i"
+		goto end
+	)
+
+:listing
+	echo RFC-files::
+	for %%i in ("*%1*") do echo %%i
+	echo.
+
+	echo RFC-index::
+	grep -i "%1" index\rfc-index.txt
+
+	goto end
+
+:help
+	echo rfc [index]
+	goto end
+
+:cleanup
+	popd >nul
+	set rfc_home=
+
+:end
