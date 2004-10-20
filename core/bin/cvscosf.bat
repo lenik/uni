daf591ae9f3de341841d66fb48e85aee48b99c8d
Lenik <pub.lenik@bodz.net>
2004-10-20 00:09:32 +0000

    [cvs] Add cvsco, which is generalized from cvscosf

--- core/bin/cvscosf.bat
+++ core/bin/cvscosf.bat
@@ -1,6 +1,6 @@
 @echo off
 
-	rem $Id: cvscosf.bat,v 1.1 2004-10-11 00:40:08 dansei Exp $
+	rem $Id: cvscosf.bat,v 1.2 2004-10-20 00:09:49 dansei Exp $
 
 	if "%1"=="" goto help
 
@@ -8,38 +8,15 @@
 
 :arg_loop
 	if "%1"=="-h" goto help
-	if "%1"=="-r" set _cvsroot=1
-	if "%1"=="-d" set _mkdir=1
+	if "%1"=="-r" set _cvsroot=-a
+	if "%1"=="-d" set _mkdir=-d
 	if "%1"=="-x" set _exit=1
 	set _prj=%1
 	shift
 	if not "%1"=="" goto arg_loop
 :arg_exit
 
-	if "%_mkdir%"=="1" (
-		if not exist "%_prj%\*" mkdir "%_prj%"
-		cd "%_prj%"
-	)
-
-	set _count=-99999
-	for /f "delims=*" %%i in ('getweb cvs.sourceforge.net http://cvs.sourceforge.net/viewcvs.py/%_prj%/') do (
-		set _line=%%i
-		set /a _count=_count+1
-		if "!_count!"=="1" (
-			set "_line=!_line:>=.!"
-			set "_line=!_line:<=.!"
-			call cmdex leftof "!_line!" "/"
-			if "!_ret!"=="CVSROOT" if not "%_cvsroot%"=="1" set _ret=
-			if not "!_ret!"=="" (
-				cvs -d :pserver:anonymous@cvs.sourceforge.net:/cvsroot/%_prj% co !_ret!
-			)
-			set _count=99999
-		)
-		if "!_line:~10,20!"=="/icons/small/dir.gif" (
-			set _count=0
-		)
-	)
-	if "%_mkdir%"=="1" cd..
+	call cvsco %_mkdir% %_cvsroot% -p %_prj%
 	goto cleanup
 
 
