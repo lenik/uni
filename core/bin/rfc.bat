fbaa39edd83327058261ab2a193345b071782faf
Lenik <pub.lenik@bodz.net>
2006-10-17 15:46:35 +0000

    [rfc index] Only assoc metapad to text type file.

--- core/bin/rfc.bat
+++ core/bin/rfc.bat
@@ -1,49 +1,55 @@
 @echo off
 
-	set rfc_home=%ARK_HOME%\standard\rfc
-	set rfc_edit=metapad
-	set rfc_name=%~1
+    set rfc_home=%ARK_HOME%\standard\rfc
+    set rfc_edit=metapad
+    set rfc_name=%~1
 
-	if "%rfc_name%"=="u" (
-		set rfc_edit=ue
-		shift
-	)
+    if "%rfc_name%"=="u" (
+        set rfc_edit=ue
+        shift
+    )
 
-	if "%rfc_name%"=="" goto help
+    if "%rfc_name%"=="" goto help
 
-	set rfc_n=%rfc_name%
-	set rfc_m=%rfc_n:~-2%
-	call libstr sn "%rfc_n%"
+    set rfc_n=%rfc_name%
+    set rfc_m=%rfc_n:~-2%
+    call libstr sn "%rfc_n%"
 
-	if "%_ret%"=="2" set rfc_p=00xx
-	if "%_ret%"=="3" set rfc_p=0%rfc_n:~0,1%xx
-	if "%_ret%"=="4" set rfc_p=%rfc_n:~0,2%xx
+    if "%_ret%"=="2" set rfc_p=00xx
+    if "%_ret%"=="3" set rfc_p=0%rfc_n:~0,1%xx
+    if "%_ret%"=="4" set rfc_p=%rfc_n:~0,2%xx
 
 :start
-	pushd "%rfc_home%" >nul
+    pushd "%rfc_home%" >nul
 
-	if exist "%rfc_p%\%rfc_name%.*" (
-		for %%i in (%rfc_p%\%rfc_name%.*) do start %rfc_edit% "%%i"
-		goto end
-	)
+    if exist "%rfc_p%\%rfc_name%.*" (
+        for %%i in (%rfc_p%\%rfc_name%.*) do (
+            if "%%~xi"==".txt" (
+                start %rfc_edit% "%%i"
+            ) else (
+                start "Loading..." "%%i"
+            )
+        )
+        goto end
+    )
 
 :listing
-	echo RFC-index::
-	grep -i "%rfc_name%" rfc-index.txt
+    echo RFC-index::
+    grep -i "%rfc_name%" rfc-index.txt
 
-	if /i not "%0"=="rfc" pause
+    if /i not "%0"=="rfc" pause
 
-	goto end
+    goto end
 
 :help
-	echo rfc [index]
-	goto end
+    echo rfc [index]
+    goto end
 
 :cleanup
-	popd >nul
-	set rfc_home=
-	set rfc_n=
-	set rfc_m=
-	set rfc_p=
+    popd >nul
+    set rfc_home=
+    set rfc_n=
+    set rfc_m=
+    set rfc_p=
 
 :end
