87eb609fbf901f7b87d71e859bb1f9100acb4328
Lenik <pub.lenik@bodz.net>
2006-10-20 10:18:39 +0000

    [six] sub scanDirectory is merged to sim.pl

--- core/lib/cmt/util.pm
+++ core/lib/cmt/util.pm
@@ -2,35 +2,17 @@
 package cmt::util;
 
 use strict;
-use DirHandle;
-use cmt::path;
+use POSIX;
 use Exporter;
 use vars qw/@ISA @EXPORT/;
 
-
-    sub scanDirectory {
-        my ($dir, $recursive, $callback, $user) = @_;
-        my $dh = new DirHandle($dir) || return;
-
-        my @files = $dh->read;
-        $dh->close;
-        for (@files) {
-            next if (m/^\.+$/);
-            my $path = "$dir/$_";
-            if (-d $path) {
-                if ($recursive) {
-                    scanDirectory($path, $recursive, $callback, $user);
-                }
-            }
-            # for each file or directory
-            &$callback($path, $user);
-        }
-    }
-
+sub datetime {
+    return strftime('%Y-%m-%d %H:%M:%S', localtime);
+}
 
 @ISA = qw(Exporter);
 @EXPORT = qw(
-	scanDirectory
+	datetime
 	);
 
 1;
