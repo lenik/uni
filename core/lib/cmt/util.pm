d74259c9703fb337ebe8640290fccd3ef9d2ae1b
Lenik <pub.lenik@bodz.net>
2006-11-16 13:49:07 +0000

    [perl] ctime + ftime => cftime

--- core/lib/cmt/util.pm
+++ core/lib/cmt/util.pm
@@ -4,15 +4,31 @@ package cmt::util;
 use strict;
 use POSIX;
 use Exporter;
+use cmt::ftime;
 use vars qw/@ISA @EXPORT/;
 
+# -> cdatetime
 sub datetime {
     return strftime('%Y-%m-%d %H:%M:%S', localtime);
+}
+
+sub cftime {
+    my $t = shift || ftime;
+    my $ms = $t - int($t);
+    # my $s = $ms + $t % 60;
+    # $t = int($t / 60);
+    # my $m = $t % 60;
+    # $t = int($t / 60);
+    # my $h = $t % 24;
+    # return sprintf("%02d:%02d:%02.6f", $h, $m, $s);
+    my $secfmt = strftime('%H:%M:%S', localtime($t));
+    return $secfmt . '.' . substr("00000$ms", -6);
 }
 
 @ISA = qw(Exporter);
 @EXPORT = qw(
 	datetime
+	cftime
 	);
 
 1;
