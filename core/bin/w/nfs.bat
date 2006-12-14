ef74f42415bb0c05b3eb9c8695b7e840a628bc61
Lenik <pub.lenik@bodz.net>
2006-12-14 11:02:14 +0000

    [nfs] Re-add w/nfs shortcut.

---
+++ core/bin/w/nfs.bat
@@ -0,0 +1,7 @@
+@echo off
+
+    rem The nfs.bat is required for launch in "Run" window.
+    rem Without this file, you need to add the .pl extension.
+    rem (That is, run "nfs.pl" instead of "nfs")
+
+    nfs.pl %*
