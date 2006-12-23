46b21013e8f9845afd34b74b7f2950d130ed2fd1
Lenik <pub.lenik@bodz.net>
2006-12-23 01:04:52 +0000

    [util] Replaced by new dcall.pl

--- core/bin/dcall.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-                              echo [%TIME%] Executing %1... 1>&2
-    if "%DCALL_VERBOSE%"=="1" echo [%TIME%] cmdline: %* 1>&2
-
-                          rem printf '[%TIME%] ' 1>&2
-                              call %*
-
-    if "%DCALL_VERBOSE%"=="1" echo [%TIME%] dcall %1 end 1>&2
