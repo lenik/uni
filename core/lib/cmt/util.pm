e52cee0e89e13a66acaac2fb33205997cb994e03
Lenik <pub.lenik@bodz.net>
2006-12-23 01:24:21 +0000

    [perl] Changed cftime to using gmtime instead of localtime. Add localftime.

--- core/lib/cmt/util.pm
+++ core/lib/cmt/util.pm
@@ -22,6 +22,21 @@ sub cftime {
     # $t = int($t / 60);
     # my $h = $t % 24;
     # return sprintf("%02d:%02d:%02.6f", $h, $m, $s);
+    my $secfmt = strftime('%H:%M:%S', gmtime($t));
+    return $secfmt . '.' . substr("00000$ms", -6);
+}
+
+sub localftime {
+    my $t = shift || ftime;
+    my $ms = $t - int($t);
+    my $dot = index($ms, '.');
+    my $ms = $dot == -1 ? 0 : substr($ms, $dot + 1, 6);
+    # my $s = $ms + $t % 60;
+    # $t = int($t / 60);
+    # my $m = $t % 60;
+    # $t = int($t / 60);
+    # my $h = $t % 24;
+    # return sprintf("%02d:%02d:%02.6f", $h, $m, $s);
     my $secfmt = strftime('%H:%M:%S', localtime($t));
     return $secfmt . '.' . substr("00000$ms", -6);
 }
