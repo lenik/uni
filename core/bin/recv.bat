da1c5fa037c26034da26404b5b7284fc4aab9f2e
Lenik <pub.lenik@bodz.net>
2007-03-04 12:16:46 +0000

    [perl net] "start cmd" if run in windows, or "exec cmd" if run in console

--- core/bin/recv.bat
+++ core/bin/recv.bat
@@ -1,5 +1,8 @@
 @echo off
 
+    set _CMDLINE=0
+    if /i "%0"=="recv" set _CMDLINE=1
+
     set _PORT=51296
     if not "%1"=="" (
         set _PORT=%1
@@ -9,7 +12,13 @@
     set _=nc %1 %2 %3 %4 %5 %6 %7 %8 %9 -L -p %_PORT%
 
     if "%verbose%"=="1" echo %_%
-    start "Recv from %_PORT%> " %_%
+
+    if "%_CMDLINE%"=="1" (
+        %_%
+    ) else (
+        start "Recv from %_PORT%> " %_%
+    )
 
 :end
     set _PORT=
+    set _CMDLINE=
--- core/bin/send.bat
+++ core/bin/send.bat
@@ -1,5 +1,8 @@
 @echo off
 
+    set _CMDLINE=0
+    if /i "%0"=="send" set _CMDLINE=1
+
     set _HOST=127.0.0.1
     set _PORT=51296
 
@@ -15,8 +18,14 @@
     set _=nc %1 %2 %3 %4 %5 %6 %7 %8 %9 %_HOST% %_PORT%
 
     if "%verbose%"=="1" echo %_%
-    start "Send to %_HOST%:%_PORT%> " %_%
+
+    if "%_CMDLINE%"=="1" (
+        %_%
+    ) else (
+        start "Send to %_HOST%:%_PORT%> " %_%
+    )
 
 :end
     set _HOST=
     set _PORT=
+    set _CMDLINE=
