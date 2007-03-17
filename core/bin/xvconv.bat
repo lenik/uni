c8a0f735fd80897d66f5b2c27991a3febb42baa6
Lenik <pub.lenik@bodz.net>
2007-03-17 05:14:21 +0000

    [xvconv] Customize bitrate: '[' <number> ']'

--- core/bin/xvconv.bat
+++ core/bin/xvconv.bat
@@ -7,7 +7,13 @@
 
     set _debug=0
     set _prefix=m6-
-    set _rotate=1
+    set _param=
+    set _param=%_param% -ofps 18.000
+    set _param=%_param% -vf-add crop=0:0:-1:-1
+    set _param=%_param% -vf-add scale=320:240
+    set _param=%_param% -vf-add expand=320:240:-1:-1:1
+    set _param=%_param% -vf-add rotate=%_rotate%
+    set _param=%_param% -xvidencopts bitrate=384
 
     if not "%1"=="[" goto begin
 :prep
@@ -16,9 +22,16 @@
         goto begin
     )
 
-    if "%1"=="d" (
+    set /a _num=%1
+    if "%_num%"=="%1" if "%2"=="]" (
+        echo set bitrate=%_num%
+        set _prefix=r%_num%-
+        set _param=-xvidencopts bitrate=%_num%
+    )
+
+    if "%1"=="k" (
         set _prefix=d-
-        set _rotate=0
+        set _param=-xvidencopts bitrate=1000
     )
 
     rem more options...
@@ -27,14 +40,29 @@
     goto prep
 
 :begin
+    call libstr sn "%_prefix%"
+    set _prefix_len=%_ret%
+
     set _fn=%~1
     if "%_fn:~0,1%"=="@" (
         set _fn=!_fn:~1!
         for /f "delims=?" %%i in (!_fn!) do (
-            echo [batch] %%i
+            echo [@ batch] %%i
             call :main "%%i"
         )
         goto end
+    )
+    if "%_fn:~0,1%"=="*" (
+        for %%i in (%_fn%) do (
+            set _i_prefix=%%i
+            set _i_prefix=!_i_prefix:~0,%_prefix_len%!
+            if "!_i_prefix!"=="%_prefix%" (
+                echo [* batch] skip %%i
+            ) else (
+                echo [* batch] %%i
+                call :main "%%i"
+            )
+        )
     )
 
 :main
@@ -42,14 +70,8 @@
 
     set menc_args=
     set menc_args=%menc_args% -mc 0
-    set menc_args=%menc_args% -ofps 18.000
-    set menc_args=%menc_args% -vf-add crop=0:0:-1:-1
-    set menc_args=%menc_args% -vf-add scale=320:240
-    set menc_args=%menc_args% -vf-add expand=320:240:-1:-1:1
-    set menc_args=%menc_args% -vf-add rotate=%_rotate%
     set menc_args=%menc_args% -srate 44100
     set menc_args=%menc_args% -ovc xvid
-    set menc_args=%menc_args% -xvidencopts bitrate=384
     set menc_args=%menc_args% -oac mp3lame
     set menc_args=%menc_args% -lameopts vbr=0
     set menc_args=%menc_args% -lameopts br=128
@@ -59,13 +81,16 @@
     set menc_args=%menc_args% -lameopts padding=3
     set menc_args=%menc_args% -af volnorm
     set menc_args=%menc_args% -xvidencopts max_bframes=0:nogmc:noqpel
+    set menc_args=%menc_args% %_param%
 
     set src=%~1
     set dst=%~dp1%_prefix%%~n1%ext%
     echo Convert %src% to %dst%...
     if "%_debug%"=="1" (
+        echo mencoder -noodml "%src%" -o "%dst%" %menc_args%
         mencoder -noodml "%src%" -o "%dst%" %menc_args%
     ) else (
+        rem echo mencoder -noodml "%src%" -o "%dst%" %menc_args%
         mencoder -noodml "%src%" -o "%dst%" %menc_args% 2>&1 |grep %% |pc -e
     )
 
