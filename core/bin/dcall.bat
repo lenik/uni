1bbd124191d13e28a6fe2e78d5e109d04a4fd3de
Lenik <pub.lenik@bodz.net>
2007-04-27 13:29:43 +0000

    [dcall] Variation dCall, DCALL for different verbose modes

--- core/bin/dcall.bat
+++ core/bin/dcall.bat
@@ -1,9 +1,24 @@
 @echo off
 
-                              echo [%TIME%] Executing %1... 1>&2
-    if "%DCALL_VERBOSE%"=="1" echo [%TIME%] cmdline: %* 1>&2
+    rem dcall   verbose=
+    rem dCall   verbose=0
+    rem DCALL   verbose=1
 
-                          rem printf '[%TIME%] ' 1>&2
-                              call %*
+    setlocal
+    set prog=%~0
+    set prog=%prog:~0,5%
+    if "%prog%"=="dCall" set DCALL_VERBOSE=0
+    if "%prog%"=="DCALL" set DCALL_VERBOSE=1
 
-    if "%DCALL_VERBOSE%"=="1" echo [%TIME%] dcall %1 end 1>&2
+    if not "%DCALL_VERBOSE%"=="0"       echo [%TIME%] Executing %1... 1>&2
+    if "%DCALL_VERBOSE%"=="1"           echo [%TIME%] cmdline: %* 1>&2
+
+    set /a _t=360000*(1%time:~0,2%-100) + 6000*(1%time:~3,2%-100) + 100*(1%time:~6,2%-100) + (1%time:~9,2%-100)
+                                        rem printf '[%TIME%] ' 1>&2
+                                        call %*
+    set /a _t=360000*(1%time:~0,2%-100) + 6000*(1%time:~3,2%-100) + 100*(1%time:~6,2%-100) + (1%time:~9,2%-100) - _t
+
+    if "%DCALL_VERBOSE%"=="1"           echo [%TIME%] dcall %1 end 1>&2
+
+    err %_t%
+    endlocal
