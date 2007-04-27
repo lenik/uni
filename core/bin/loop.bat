e4df9a34a433d40a18793e3d686450a460ca6c82
Lenik <pub.lenik@bodz.net>
2007-04-27 13:45:26 +0000

    [loop] Add delay option to loop.bat

--- core/bin/loop.bat
+++ core/bin/loop.bat
@@ -1,7 +1,40 @@
 @echo off
-title "%*"
+
+    setlocal
+
+    if not "%1"=="[" goto prep2
+:prep
+    if "%1"=="]" (
+        shift
+        goto prep2
+    )
+
+    set /a _num=%1
+    if "%_num%"=="%1" if "%2"=="]" (
+        set _delay=%_num%
+    )
+
+    rem more options...
+
+    shift
+    goto prep
+
+:prep2
+    if "%1"=="" goto begin
+    set _cl=%_cl% %1
+    shift
+    goto prep2
+
+:begin
+    title "%_cl%"
 
 :loop
-	call %*
+	call %_cl%
 
-goto loop
+    if not "%_delay%"=="" (
+        sleep -m %_delay%
+    )
+
+    goto loop
+
+:end
