28d871ec010c3d3d47947269dbbe00ff1c424bc9
Lenik <pub.lenik@bodz.net>
2007-06-23 04:56:09 +0000

    [realplay] rmclar bugfix.

--- core/bin/rmclear.bat
+++ core/bin/rmclear.bat
@@ -1,12 +1,12 @@
 @echo off
 
-rmevents -i "%1" -o "%1.tmp" -e nul
-if not exist "%1.tmp" (
+rmevents -i "%~1" -o "%~1.tmp" -e nul
+if not exist "%~1.tmp" (
 	echo failed to clear events
 	goto end
 )
 
-del "%1"
-move "%1.tmp" "%1"
+move "%~1" "%~1.bak"
+move "%~1.tmp" "%~1"
 
 :end
