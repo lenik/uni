f630b25a4c832e99d936ff80c121a9370dc06704
Lenik <pub.lenik@bodz.net>
2007-06-30 09:50:16 +0000

    [loop bat] Remove most of options.

--- core/bin/loop.bat
+++ core/bin/loop.bat
@@ -2,11 +2,35 @@
 
     setlocal
 
-    if not "%1"=="[" goto prep2
 :prep
+    if "%1"=="[" goto get_delay
+    if "%1"=="--" (
+        shift
+        goto get_cl
+    ) else if "%1"=="-c" (
+        set _onchange=1
+    ) else if "%1"=="-d" (
+        set _runcopy=1
+    ) else if "%1"=="-k" (
+        set _fork=1
+    ) else if "%1"=="-cdk" (
+        set _onchange=1
+        set _runcopy=1
+        set _fork=1
+    ) else if "%1"=="-v" (
+        set _verbose=1
+    ) else if "%1"=="-h" (
+        goto help
+    ) else (
+        goto get_cl
+    )
+    shift
+    goto prep
+
+:get_delay
     if "%1"=="]" (
         shift
-        goto prep2
+        goto prep
     )
 
     set /a _num=%1
@@ -15,26 +39,82 @@
     )
 
     rem more options...
-
     shift
-    goto prep
+    goto get_delay
 
-:prep2
-    if "%1"=="" goto begin
+:get_cl
+    if "%~1"=="" goto help
+    set _srcprog=%~1
+    set _runprog=%~1
+    if "%_runcopy%"=="1" set _runprog=%~dpn1.copied%~x1
+    if "%_verbose%"=="1" (
+        echo [loop] src-program: %_srcprog%
+        echo [loop] run-program: %_runprog%
+    )
+    set _cl=%_runprog%
+    shift
+
+:get_cl_args
+    if "%~1"=="" goto begin
     set _cl=%_cl% %1
     shift
-    goto prep2
+    goto get_cl_args
+
+:help
+    echo [loop] Running command repeatly
+    echo Written by Lenik,  $Revision: 1.6 $
+    echo Syntax:
+    echo     loop [options] command arguments...
+    echo Options:
+    echo     ['[' delay ']']:
+    echo            interval delay ^(seconds^)
+    echo     -c:    repeat on program-file changes ^(Modified-Time^)
+    echo     -d:    dup program before running
+    echo     -k:    fork mode
+    echo     -cdk:  set -c, -d, -k
+    echo     -h:    display this help page
+    echo     -v:    display verbose information
+    goto end
 
 :begin
-    title "%_cl%"
+    if "%_onchange%"=="1" (
+        call filetime "%_srcprog%"
+        set _last=!errorlevel!
+        if "%_verbose%"=="1" echo [loop] Initial version of %_srcprog%: !_last!
+    )
+
+    if "%_verbose%"=="1" title %_cl%
 
 :loop
-	call %_cl%
+
+    if "%_verbose%"=="1" echo [loop] execute: %_cl%
+    copy /y "%_srcprog%" "%_runprog%" >nul
+    call %_cl%
+
+    set _keygot=0
+
+    if not "%_onchange%"=="1" goto skip_waitch
+
+:waitch
+    set _cur=%~t1
+    REM if %_cur%
+:skip_waitch
 
     if not "%_delay%"=="" (
-        sleep -m %_delay%
+        readkey -t %_delay%
+        if errorlevel 1 set _keygot=1
+        if "!errorlevel!"=="113" goto quit
     )
 
     goto loop
+
+:quit
+    echo Quitting...
+
+:cleanup
+    if "%_runcopy%"=="1" (
+        if "%_verbose%"=="1" echo [loop] remove copy: %_runprog%
+        del "%_runprog%" >nul
+    )
 
 :end
