5381a53e056e6f47343af08ab5c14eada28699e5
Lenik <pub.lenik@bodz.net>
2007-07-11 15:25:09 +0000

    [perl fun] Add xs/~stwrap

--- core/lib/fun/xs.pm
+++ core/lib/fun/xs.pm
@@ -17,15 +17,17 @@ sub info2;
 sub version;
 sub help;
 
-our $opt_verbtitle      = 'xsutil';
-our $opt_verbtime       = 0;
-our $opt_verbose        = 1;
+my $opt_verbtitle       = 'xsutil';
+my $opt_verbtime        = 0;
+my $opt_verbose         = 1;
+my $opt_package;
 
 sub boot {
     GetOptions('quiet|q'    => sub { $opt_verbose-- },
                'verbose|v'  => sub { $opt_verbose++ },
                'version'    => sub { version; exit },
                'help|h'     => sub { help; exit },
+               'package|p=s',
                );
 }
 
@@ -44,7 +46,7 @@ sub info2 {
 }
 
 sub version {
-    my %id = parse_id('$Id: xs.pm,v 1.1 2007-07-10 15:43:00 lenik Exp $');
+    my %id = parse_id('$Id: xs.pm,v 1.2 2007-07-11 15:25:09 lenik Exp $');
     print "[$opt_verbtitle] XSUB Utilities \n";
     print "Written by Lenik,  Version $id{rev},  Last updated at $id{date}\n";
 }
@@ -57,6 +59,7 @@ Syntax:
         fun ~command <options>...
 
 Common Options:
+        --package=<perl-package> (p, eg. "MyPkg::MySubPkg")
         --quiet (q)
         --verbose (v, repeat twice give you more verbose info)
         --version
@@ -68,9 +71,28 @@ EOM
 }
 
 sub stwrap {
+    my $stname;
+    my %st;
+
+    sub stdump {
+        my ($n, $s) = @_;
+        info "dump: $n => $s";
+    }
 
     while (<>) {
+        if (/struct \s+ (\w+)/x) {
+            stdump $stname, \%st if defined $stname;
+            $stname = $1;
+            undef %st;
+            next;
+        }
+        while (/^\s* (.+) \s+ (\w+ (,\s*\w+)* ) \s* ;/g) {
+            my $type = $1;
+            my @mems = split(/\s*,\s*/, $2);
+            info "$type: [".join('|', @mems).']';
+        }
     }
+    stdump $stname, \%st if defined $stname;
 }
 
 1
