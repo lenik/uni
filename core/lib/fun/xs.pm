4c6feb1c769afa48aa428791432cd0d5a3d5f538
Lenik <pub.lenik@bodz.net>
2007-07-15 07:58:35 +0000

    [perl xsub] fun::xs initial release.

--- core/lib/fun/xs.pm
+++ core/lib/fun/xs.pm
@@ -13,6 +13,7 @@ use YAML;
 @ISA    = qw(Exporter);
 @EXPORT = qw(
              stwrap
+             typemap
              );
 
 sub boot;
@@ -32,9 +33,12 @@ our $opt_package;
 our $opt_pkgprefix;
 our $opt_before;
 our $opt_after;
-our %opt_struct_only;
+our %opt_select;
+our $opt_readonly       = 0;
 
-my $TYPES;
+my $TYPES;              # $typename -> [$orig_typename, $stype, @dim_spec]
+my @STYPES;             # ( [\%levels, \%mm]* )
+my %WHICHST;
 
 sub boot {
     GetOptions('quiet|q'    => sub { $opt_verbose-- },
@@ -44,7 +48,8 @@ sub boot {
                'package|p=s',
                'before|b=s',
                'after|a=s',
-               'struct=s'   => sub { $opt_struct_only{$_[1]} = 1 },
+               'select=s'   => sub { $opt_select{$_[1]} = 1 },
+               'readonly',
                );
     if (defined $opt_package) {
         $opt_pkgprefix = $opt_package;
@@ -75,7 +80,7 @@ sub info2 {
 }
 
 sub version {
-    my %id = parse_id('$Id: xs.pm,v 1.4 2007-07-15 00:30:16 lenik Exp $');
+    my %id = parse_id('$Id: xs.pm,v 1.5 2007-07-15 07:58:35 lenik Exp $');
     print "[$opt_verbtitle] XSUB Utilities \n";
     print "Written by Lenik,  Version $id{rev},  Last updated at $id{date}\n";
 }
@@ -98,38 +103,66 @@ Common Options:
 
 Commands:
         ~stwrap     C-Struct Wrapper
-                    --struct=<name> (output only specified structs)
+                    --select=<name> (output only select structs)
+                    --readonly
 EOM
 }
 
 sub stwrap {
     my $name;
+    my $levels = [[], []];
     my %mm;
+    my $st = [ $levels, \%mm ];
 
     ppcmt {
+      # struct: dump and create
         # struct (XXX)
         if (/struct \s+ (\w*) (?!.*;)/x) {
+            push @STYPES, $st;
             stdump $name, \%mm if defined $name;
-            $name = $1;
-            undef %mm;
+            if ($1 ne '') {
+                $name   = "struct $1 *";
+                $levels = [["struct $1"], ["struct $1 *"]];
+            } else {
+                undef $name;
+                $levels = [[], []];
+            }
+            %mm = ();
+            $st = [ $levels, \%mm ];
             next;
         }
 
+      # typedef: dump and clean
         # typedef... } (*XXX (, *XXX)) ;
         if (/\}\s*(\**\w+(?:\s*,\s*\**\w+)*)\s*;/) {
-            my @typedefs = split(/\s*,\s*/, $1);
+            my @typedefs = map { s/\s+//g; $_ } split(/\s*,\s*/, $1);
             for (@typedefs) {
-                next if /^\*/;      # skip ptr typedefs.
-                stdump $_, \%mm;
+                /^(\**)(\w+)$/;
+                my $level = length $1;
+                # unshift so prefer over "struct XXX *"
+                unshift @{$levels->[$level]}, $2;
+            }
+            push @STYPES, $st;
+
+            # prefer to dump "LPXXX" than "XXX *"
+            if (@{$levels->[1]}) {
+                stdump $_, \%mm for @{$levels->[1]};
+            } elsif (@{$levels->[0]}) {
+                stdump "$_ *", \%mm for @{$levels->[0]};
+            } else {
+                # ignore this
             }
             undef $name;
-            undef %mm;
+            $levels = [[], []];
+            %mm = ();
+            $st = [ $levels, \%mm ];
             next;
         }
 
-        # (NAME) ([10][20])
-        my $_var = qr/(\w+)((?:\s*\[\s*\w*\s*\])*)/;
+        # (NAME) ([][1+2])
+        my $_var = qr/(\w+)((?:\s*\[[^\[\]]*\])*)/;
 
+      # add members
         # (long long int *) (MEMBER (, MEMBER2));
         while (/^\s* (.+?) \b ($_var(?:,\s*$_var)*) \s*;/xg) {
             my $type = $1;
@@ -140,67 +173,120 @@ sub stwrap {
                     unless /^$_var$/;
                 my $nam = $1; $_ = $2;
                 my @dim;
-                push @dim, $1 while /\[\s*(\w*)\s*\]/g;
-                $mm{$nam} = addtype($type, @dim);
+                push @dim, $1 while /\[([^\[\]]*)\]/g;
+                $mm{$nam} = addtype($st, $type, @dim);
                 push @{$mm{'<'}}, $nam;
             }
         }
     };
+    push @STYPES, $st;
     stdump $name, \%mm if defined $name;
     typedump;
 }
 
-sub INDENTED {
-    my @lines = split(/\n/, shift);
-    my ($s) = ($lines[0] =~ /^(\s*)/);
-    my $l = length $s;
-    join("\n", map { substr($_, $l) } @lines);
-}
-
 sub stdump {
-    my ($n, $mm) = @_;
-    return if (%opt_struct_only and ! defined $opt_struct_only{$n});
-
-    info2 "dump: $n => ".Dumper($mm);
+    my ($typename, $mm) = @_;
+    return if (defined %opt_select and ! defined $opt_select{$typename});
 
     # (lib_stname_)member
-    my $prefix = $opt_pkgprefix.$n.'_';
+    my $prefix = $opt_pkgprefix.$typename.'_';
     for (@{$mm->{'<'}}) {
         my ($nam, $type) = ($_, $mm->{$_});
-        my $chunk = INDENTED<<"EOM";
-            $type
-            ${prefix}$nam(self)
-                    struct $n *self;
-                CODE:
-                    RETVAL = self->$nam;
-                OUTPUT:
-                    RETVAL
-
-            void
-            ${prefix}set$nam(self, $type newval)
-                    struct $n *self;
-                CODE:
-                    self->$nam = newval;
-                OUTPUT:
-                    self
-
-
-EOM
+        my $origtype    = $TYPES->{$type}->[0];
+        my $st          = $TYPES->{$type}->[1];
+        my $is_array    = defined $TYPES->{$type}->[2];
+        if (defined $st) {
+            my $l = getlevel($origtype, $st->[0]);
+            if ($l >= 0) {
+                my $level_prefer = $st->[0]->[$l]->[0];
+                if (defined $level_prefer) {
+                    $type = $level_prefer;
+                }
+            }
+        }
+        my $chunk = join("\n",
+            "$type",
+            "$nam(self)",
+            "        $typename self",
+            "    PROTOTYPE: \$",
+            "    CODE: ",
+            $is_array
+                ? "        memcpy(RETVAL, self->$nam, sizeof($type)); "
+                : "        RETVAL = self->$nam; ",
+            "    OUTPUT: ",
+            "        RETVAL",
+            $opt_readonly
+                ? ()
+                : ("",
+                    "void",
+                    "set_$nam(self, $type newval)",
+                    "        $typename self",
+                    "    PROTOTYPE: \$\$",
+                    "    CODE: ",
+                    $is_array
+                        ? "        memcpy(self->$nam, newval, sizeof($type)); "
+                        : "        self->$nam = newval; ",
+                    "    OUTPUT: ",
+                    "        self",
+                ),
+            "\n",
+            );
         print $chunk;
     }
 }
 
+sub getlevel {
+    my ($typename, $levels) = @_;
+    my $incl = 0;
+    if ($typename =~ s/((\*|\s)+)$//) {   # remove trailing *
+        my $ast = $1;
+        $ast =~ s/\s//g;
+        $incl = length $ast;
+    }
+    $typename =~ s/(^\s+)|(\s+$)//g;    # trim
+    $typename =~ s/\s\s+/ /g;           # normalize space
+    for my $l (0..$#$levels) {
+        my $level = $levels->[$l];
+        next unless defined $level;
+        for (@$level) {
+            return $l + $incl if $_ eq $typename;
+        }
+    }
+    return -1;
+}
+
+
+sub whichst {
+    my ($type, $st) = @_;   # holding the stype under parsing
+    return $WHICHST{$type} if exists $WHICHST{$type};
+    my $l = getlevel($type, $st->[0]);
+    if ($l < 0) {
+        undef $st;
+        for (@STYPES) {
+            $l = getlevel($type, $_->[0]);
+            if ($l >= 0) {
+                $st = $_;
+                last;
+            }
+        }
+    }
+    return wantarray ? ($st, $l) : $st;
+}
+
 sub addtype {
-    my $type = shift; # @_ == @dim
+    my $st      = shift;    # holding the stype under parsing
+    my $type    = shift;    # @_ == @dim
     # struct|class TYPE **
-    $type =~ s/(^\s+)|(\s+$)//g;
-    $type =~ s/\s+/_/g;
-    my $orig = [$type, @_];
+    $type       =~ s/(^\s+)|(\s+$)//g;
+    $type       =~ s/\s+/ /g;
 
-    $type =~ s/\W/_/g;
+    $st         = whichst($type, $st);
+    my $orig    = [$type, $st, @_];
 
+    $type       =~ s/ /_/g;
+    $type       =~ s/\W/_/g;
     for (@_) {
-        s/\s+/_/g;
+        s/\s+//g;   # space in dim-spec is removed
         s/\W/_/g;
         $type .= '_'.$_;
     }
@@ -224,6 +310,36 @@ sub typedump {
             # typedef $tname $type [$dim]...;
             $typedef .= ' ';
             $typedef .= "[$_]" for @$info[1..$#$info];
+        }
+    }
+}
+
+# generate Module.typemap.xsh XS-include file
+sub typemap {
+    my %SECTION;
+    ppcmt {
+        # MODULE=XXX::YYY   PACKAGE=XXX::YYY    PREFIX=XXX
+        if (/^MODULE\s*=\s*(\S+)/) {
+            while (/(\w+)\s*=\s*(\S+)/g) {
+                $SECTION{$1} = $2;
+            }
+            next;
+        }
+
+        # TYPEMAP(type)
+        if (/TYPEMAP\s*\(\s*(\w+)\s*\)/) {
+            my $type = $1;
+            print <<"EOM";
+$type
+_typemap_${type}_output(var)
+        UV var
+    PROTOTYPE: \$
+    CODE:
+        RETVAL = ($type) var;
+    OUTPUT:
+        RETVAL
+
+EOM
         }
     }
 }
