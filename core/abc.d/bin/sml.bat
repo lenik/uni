f225530fd910ef20bbe73b995357efdbdca1140f
Lenik <pub.lenik@bodz.net>
2007-07-16 12:06:23 +0000

    [alias] Using 'start "big"' to have a big-font window

--- core/abc.d/bin/sml.bat
+++ core/abc.d/bin/sml.bat
@@ -9,7 +9,7 @@ if "%0"=="ml" goto manual
 if "%0"=="ML" goto manual
 
 :auto
-    start "Standard ML" "%SMLNJ_HOME%\bin\sml" %1 %2 %3 %4 %5 %6 %7 %8 %9
+    start "Big" "%SMLNJ_HOME%\bin\sml" %1 %2 %3 %4 %5 %6 %7 %8 %9
     goto end
 
 :manual
