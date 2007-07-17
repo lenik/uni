2947b625855db0693a3e2a0388387f38cfd99ae1
Lenik <pub.lenik@bodz.net>
2007-07-17 11:00:13 +0000

    [perl cmt] Using prototype() to avoid the function absorb arguments.

--- core/lib/cmt/ftime.pm
+++ core/lib/cmt/ftime.pm
@@ -6,18 +6,18 @@ use Time::HiRes qw/time usleep/;
 use Exporter;
 use vars qw/@ISA @EXPORT/;
 
-# the rhs is fixed to the common error:
-# when used in
+# the prototype () is required, to avoid
 #       $elaps = ftime - $last_time;
-# will be parsed as
+# be parsed as
 #       $elaps = ftime(-$last_time);
-sub ftime {
-    my $rhs = shift || 0;
-    return time + $rhs;
+sub ftime() {
+    # my $rhs = shift || 0;
+    return time; # + $rhs;
 }
 
 sub fsleep {
     my $fsec = shift;
+    return if $fsec <= 0;
     my $us = 1000000 * $fsec;
     usleep($us);
 }
