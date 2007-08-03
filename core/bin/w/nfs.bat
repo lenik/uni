2e476e28195391bb3fd9b643d15bcd4f4d921f57
Lenik <pub.lenik@bodz.net>
2007-08-03 14:52:37 +0000

    [nfs] Add bat wrapper.

--- core/bin/w/nfs.bat
+++ core/bin/w/nfs.bat
@@ -1,7 +1,26 @@
 @echo off
 
-    rem The nfs.bat is required for launch in "Run" window.
-    rem Without this file, you need to add the .pl extension.
-    rem (That is, run "nfs.pl" instead of "nfs")
+    if not "%OS%"=="Windows_NT" goto err_os
 
-    nfs.pl %*
+    setlocal
+    set _pl=%~dpn0.pl
+    if not exist "%_pl%" (
+        for %%i in (p pc pld) do (
+            if exist "%~dpn0.%%i" (
+                set _pl=%~dpn0.%%i
+                goto start
+            )
+        )
+        echo is file %~dpn0.pl lost?
+        goto end
+    )
+
+:start
+    %perl% "%_pl%" %*
+    goto end
+
+:err_os
+	echo You must run this program under Windows NT/2000 or above.
+	goto end
+
+:end
