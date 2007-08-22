dc0d4e6f1552704b93ec89406111b886b69d3a63
Lenik <pub.lenik@bodz.net>
2007-08-22 15:41:09 +0000

    [perl cmt] cmt::lexutil impl.
    - unified the lex/lexer/yylex name convention
    - if the last token __xxx is returned after EOF, then the token is changed to EOF-token.
    - moved lex_dump from cmt::oop to cmt::lexutil

--- core/lib/cmt/lexutil.pm
+++ core/lib/cmt/lexutil.pm
@@ -2,25 +2,36 @@ package cmt::lexutil;       # helpers for Parse::Lex
 
 use strict;
 use Exporter;
+use UNIVERSAL('isa');
 
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(yylex2
+                 lex_dump
                  );
 
-# ($toknam, $tokval) = yylex2($lexer)->($text)->()
+# $plex     = Parse::Lex
+# $lexer    = yylex2($plex)
+# $yylex    = $lexer->($text)
+
+# ($toknam, $tokval) = yylex2($plex)->($text)->()
 sub yylex2 {
-    my $lex = shift;
+    my $plex = shift;
     sub {
         my $text = shift;
-        return $lex unless defined $text;
+        return $plex unless defined $text;
 
-        $lex->from($text);
+        $plex->from($text);
         sub {
             my $t;
             my ($n, $v);
           T:while (1) {
-                $t = $lex->next;
-                last if !defined($t) || $lex->eoi;
+                $t = $plex->next;
+                if (!defined($t) || $plex->eoi) {
+                    if (substr($n, 0, 2) eq '__') {
+                        ($n, $v) = ('', undef);
+                    }
+                    last
+                }
                 $n = $t->name;
                 $v = $t->text;
               N:while (1) {
@@ -41,6 +52,30 @@ sub yylex2 {
             $n = '_'.$n if $n =~ /^[a-zA-Z]/;
             ($n, $v)
         }
+    }
+}
+
+sub lex_dump {
+    my $what = shift;
+    my $yylex;
+    if (isa($what, 'Parse::Lex')) {
+        my $plex = $what;
+        $yylex = sub {
+            my $t = $plex->next;
+            if ($plex->eoi || !defined $t) {
+                ('', undef)
+            } else {
+                ($t->name, $t->text)
+            }
+        };
+    } elsif (@_) {
+        $yylex = $what->(shift);
+    } else {
+        $yylex = $what;
+    }
+    while (my @ret = $yylex->()) {
+        last if $ret[0] eq '';
+        printf "%-10s '%s'\n", @ret;
     }
 }
 
