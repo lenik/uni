94bb94139fc80fe62896e5dcd49e47e521770988
Lenik <pub.lenik@bodz.net>
2007-08-24 10:28:59 +0000

    [bat util] Test end-of-arglist using "-translate method. (still not complete)

--- core/bin/args.bat
+++ core/bin/args.bat
@@ -1,28 +1,21 @@
 @echo off
 
-	set _arg_i=0
-	set /a _arg_max=%1
-
-	if %_arg_max% gtr 1 goto start
-	set _arg_max=9
+    setlocal
 
 :start
-	echo Arg-*: %*
-	echo Arg-0: %0
+	echo arg.* = %*
+	echo arg.0 = %0
 
 :loop
-	if %_arg_i% geq %_arg_max% goto loop_x
-
-	set /a _arg_i=_arg_i+1
-
-	echo Arg-%_arg_i%: %1
-
+    if "%~1"=="" (
+        set _=%1.
+        set _=!_:"=?!
+        if !_!==. goto end
+	)
+	set /a i = i + 1
+	echo arg.%i% = `%1'
+	echo                                 ~ `%~1'
 	shift
-
-goto loop
-
-:loop_x
-	set _arg_i=
-	set _arg_max=
+    goto loop
 
 :end
