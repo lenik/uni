3bae6a994360339592ca229bf7653fabe3bce1a7
Lenik <pub.lenik@bodz.net>
2007-08-27 23:50:53 +0000

    [labat] Add _compile, _stdfun_proto, _arg_unzip.

--- core/lib/labat.pm
+++ core/lib/labat.pm
@@ -1,4 +1,4 @@
-package cmt::labat;     # package description
+package labat;          # Lapiota Batch-Processing
 
 use strict;
 use cmt::util;
@@ -10,28 +10,72 @@ our $opt_verbose        = 1;
 
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(labat_compile
-                 labat_eval
                  );
 
-our %ops;
-
 sub labat_compile {
-    # TODO
+    my $root = shift;
+    my $cat = {};
+    my $ctx = {};
 }
 
-sub labat_eval {
+sub _compile {
+    my ($cat, $fun, $node);
+    if ($fun =~ /^:(\S+)$/) {
+        my $cat_name = $1;
+        my $subcat = $cat->{$cat_name} = {};
+        _compile($subcat,
+        # :: description node should be filtered out.
+    } else {
+        die "unknown fun-name: $fun" unless defined $funs->{$fun};
+        $fun_proto = $funs->{$fun};
+        $fun_proto->($node);
+    }
+
+sub _stdfun_proto {
+    my ($fun_name, $arg_node, $funs, $vars) = @_;
+    my $code =
+    die "fun $fun_name isn't existed" unless ref $funs->{$fun_name};
+    $fun_name
+    my @calls_with = _arg_unzip($arg_node);
+    return undef unless @calls_with;
+    my @calls;
+    push @calls, 'do-seq' if @calls_with > 1;
+    for (@calls_with) {
+        push @calls, [ $fun, @$_ ];
+    }
 }
 
-sub set_env {
-}
-
-sub add_opentype {
-}
-
-sub set_assoc {
-}
-
-sub set_reg {
+sub _arg_unzip {
+    my $node = shift;
+    return $node unless (ref $node eq 'ARRAY');
+    my @list;
+    my @prefix;
+    local $_;
+    for (my $i = 0; $i < @$node; $i++) {
+        $_ = $node->[$i];
+        if (ref $_ eq 'ARRAY') {
+            if (@prefix) {
+                my @sublist = _arg_unzip($_);
+                for (@sublist) {
+                    push @list, [ @prefix, $_ ];
+                }
+                undef @prefix;
+            } else {
+                push @list, [ $_ ];
+            }
+        } elsif (not ref $_ and s/->$//) {
+            push @prefix, $_;
+        } else {
+            # non-ARRAY-ref or scalar
+            if (@prefix) {
+                push @list, [ @prefix, $_ ];
+                undef @prefix;
+            } else {
+                push @list, $_;
+            }
+        }
+    }
+    return @list;
 }
 
 1
