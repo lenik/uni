213cca6af757fa76931e15ca4b8cad2e4e5d073d
Lenik <pub.lenik@bodz.net>
2007-08-27 23:40:22 +0000

    [labbat] Add labat::win32 and labat/.pm template file.

---
+++ core/lib/labat/.pm
@@ -0,0 +1,19 @@
+package cmt::XXX;       # package description
+
+use strict;
+use cmt::util;
+use Exporter;
+
+our $opt_verbtitle      = __PACKAGE__;
+our $opt_verbtime       = 0;
+our $opt_verbose        = 1;
+
+our @ISA    = qw(Exporter);
+our @EXPORT = qw(mysub
+                 );
+
+sub mysub {
+    # TODO
+}
+
+1
---
+++ core/lib/labat/win32.pm
@@ -0,0 +1,29 @@
+package cmt::XXX;       # package description
+
+use strict;
+use cmt::util;
+use Exporter;
+use labat;
+
+our $opt_verbtitle      = __PACKAGE__;
+our $opt_verbtime       = 0;
+our $opt_verbose        = 1;
+
+our @ISA    = qw(Exporter);
+our @EXPORT = qw(mysub
+                 );
+
+
+sub set_env {
+}
+
+sub add_opentype {
+}
+
+sub set_assoc {
+}
+
+sub set_reg {
+}
+
+1
