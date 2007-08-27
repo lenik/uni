5e634b7c6271367d82dc30f4cdaf7d7e7c6febe6
Lenik <pub.lenik@bodz.net>
2007-08-27 11:16:28 +0000

    [labat] Add labat.

---
+++ core/lib/labat.pm
@@ -0,0 +1,37 @@
+package cmt::labat;     # package description
+
+use strict;
+use cmt::util;
+use Exporter;
+
+our $opt_verbtitle      = __PACKAGE__;
+our $opt_verbtime       = 0;
+our $opt_verbose        = 1;
+
+our @ISA    = qw(Exporter);
+our @EXPORT = qw(labat_compile
+                 labat_eval
+                 );
+
+our %ops;
+
+sub labat_compile {
+    # TODO
+}
+
+sub labat_eval {
+}
+
+sub set_env {
+}
+
+sub add_opentype {
+}
+
+sub set_assoc {
+}
+
+sub set_reg {
+}
+
+1
