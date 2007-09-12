7b756df8597df0f853f1224547c2b97e043b9d16
Lenik <pub.lenik@bodz.net>
2007-09-12 16:12:40 +0000

    [perl template] Add pod framework and other stuff.
    - move info() to cmt::log::_log()
    - add _sig()
    - add pod framework
    - import by functions

--- core/bin/.pl
+++ core/bin/.pl
@@ -1,58 +1,99 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+UnKnOwN - Perl_simple_cli_program_template
+
+=cut
 use strict;
-use cmt::util;
-use cmt::vcs;
+use cmt::log;
+use cmt::vcs('parse_id');
+    my %RCSID = parse_id('$Id: .pl,v 1.11 2007-09-12 16:12:40 lenik Exp $');
 use Getopt::Long;
 
-sub boot;
-sub info;
-sub info2;
-sub version;
-sub help;
-sub _main;
+sub _main; sub _version; sub _help;
 
-our $opt_verbtitle      = 'unknown';
-our $opt_verbtime       = 0;
-our $opt_verbose        = 1;
+BEGIN {
+    ($opt_verbtitle) = 'UnKnOwN'; # $0 =~ /([^\/\\.]+)(?:\.\w+)*$/;
+    sub _log2 { $opt_verbose >= 2 && &_log }
+    sub _log3 { return if $opt_verbose < 3; &_log }
+    sub _sig2 { return if $opt_verbose < 2; &_sig }
+    sub _sig3 { return if $opt_verbose < 3; &_sig }
+}
 
 sub boot {
     GetOptions('quiet|q'    => sub { $opt_verbose-- },
                'verbose|v'  => sub { $opt_verbose++ },
-               'version'    => sub { version; exit },
-               'help|h'     => sub { help; exit },
+               'version'    => sub { _version; exit 0 },
+               'help|h'     => sub { _help; exit 0 },
                );
     _main;
 }
 
-sub info {
-    return if $opt_verbose < 1;
-    my $text = shift;
-    print datetime.' ' if $opt_verbtime;
-    print "[$opt_verbtitle] $text\n";
-}
+=head1 SYNOPSIS
 
-sub info2 {
-    return if $opt_verbose < 2;
-    my $text = shift;
-    print datetime.' ' if $opt_verbtime;
-    print "[$opt_verbtitle] $text\n";
-}
+B<UnKnOwN>
+    S<[ B<-q> | B<--quiet> ]>
+    S<[ B<-v> | B<--verbose> ]>
+    S<[ B<-h> | B<--help> ]>
+    S<[ B<--version> ]>
+    S<[ B<--> ]>
+    S<[ I<...the rest of arguments...> ]>
 
-sub version {
-    my %id = parse_id('$Id: .pl,v 1.10 2007-08-26 12:42:33 lenik Exp $');
-    print "[$opt_verbtitle] Perl_simple_cli_program_template \n";
-    print "Written by Lenik,  Version $id{rev},  Last updated at $id{date}\n";
-}
+=head1 DESCRIPTION
 
-sub help {
-    version;
+B<UnKnOwN> is a WHAT used for WHAT. It HOW-WORKS.
+
+BACKGROUND-PROBLEM.
+
+HOW-UnKnOwN-RESOLVES.
+
+=head1 OPTIONS
+
+=over 8
+
+=item -m, --myoptions
+
+...
+
+=item -q, --quiet
+
+Repeat this option to suppress unimportant information to display.
+
+=item -v, --verbose
+
+Repeat this option to display more detailed information.
+
+=item -h, --help
+
+Display a breif help page and exit(0).
+
+=item --version
+
+Display a short version information and exit(0).
+
+=back
+
+=head1 ENVIRONMENT
+
+=over 8
+
+=item TEMP, TMP
+
+TEMP(or TMP, if $TEMP directory isn't existed) directory used to create
+temporary files.
+
+=back
+
+=cut
+sub _help {
+    &_version;
     print "\nSyntax: \n    $0 [OPTION] [--] ...\n", <<'EOM';
 
 Common options:
-    -h, --help              show this help page
     -q, --quiet             repeat to get less info
     -v, --verbose           repeat to get more info
+    -h, --help              show this help page
         --version           print the version info
 EOM
 }
@@ -60,5 +101,46 @@ EOM
 exit boot;
 
 sub _main {
-    info "TODO...";
+    _sig 'ARG', $_ for @ARGV;
+    _log "TODO...";
 }
+
+=head1 DIAGNOSTICS
+
+(No Information)
+
+=cut
+# (HELPER FUNCTIONS)
+
+=head1 HACKING
+
+(No Information)
+
+=cut
+# (MODULE FUNCTIONS)
+
+=head1 HISTORY
+
+=over
+
+=item 0.x
+
+The initial version.
+
+=back
+
+=cut
+sub _version {
+    print "[$opt_verbtitle] Perl_simple_cli_program_template \n";
+    print "Written by Lenik,  Version 0.$RCSID{rev},  Last updated at $RCSID{date}\n";
+}
+
+=head1 SEE ALSO
+
+The L<cmt/"Perl_simple_cli_program_template">
+
+=head1 AUTHOR
+
+Xima Lenik <lenik@bodz.net>
+
+=cut
