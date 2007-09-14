e9db066dff8d83442e93c264336ed1ecebd093fb
Lenik <pub.lenik@bodz.net>
2007-09-14 16:09:45 +0000

    [perl cmt] cmt-template changes.
    - use cmt::log(2)
    - add pod framework

--- core/lib/cmt/.pm
+++ core/lib/cmt/.pm
@@ -1,19 +1,74 @@
-package cmt::XXX;       # package description
+package cmt::UnKnOwN;
 
+=head1 NAME
+
+UnKnOwN - Perl_simple_module_template
+
+=cut
 use strict;
+use vars qw($LOGNAME $LOGLEVEL);
+    $LOGNAME    = __PACKAGE__;
+    $LOGLEVEL   = 0;
+use cmt::log(2);
 use cmt::util;
+use cmt::vcs('parse_id');
+    my %RCSID   = parse_id('$Id: .pm,v 1.7 2007-09-14 16:09:45 lenik Exp $');
+    our $VER    = "0.$RCSID{rev}";
 use Exporter;
-
-our $opt_verbtitle      = __PACKAGE__;
-our $opt_verbtime       = 0;
-our $opt_verbose        = 1;
 
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(mysub
                  );
 
+# INITIALIZORS
+
+=head1 SYNOPSIS
+
+    use UnKnOwN;
+    mysub(arguments...)
+
+=head1 DESCRIPTION
+
+B<UnKnOwN> is a WHAT used for WHAT. It HOW-WORKS.
+
+BACKGROUND-PROBLEM.
+
+HOW-UnKnOwN-RESOLVES.
+
+=head1 FUNCTIONS
+
+=cut
+=head2 mysub(arguments)
+
+=cut
 sub mysub {
     # TODO
 }
 
+=head1 DIAGNOSTICS
+
+(No Information)
+
+=cut
+# (HELPER FUNCTIONS)
+
+=head1 HISTORY
+
+=over
+
+=item 0.x
+
+The initial version.
+
+=back
+
+=head1 SEE ALSO
+
+The L<cmt/"Perl_simple_module_template">
+
+=head1 AUTHOR
+
+Xima Lenik <name@mail.box>
+
+=cut
 1
