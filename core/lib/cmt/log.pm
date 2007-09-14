5efe87778ddcd730ecd494777ab8b71f97e71da2
Lenik <pub.lenik@bodz.net>
2007-09-14 15:40:08 +0000

    [labat cmt] cmt::log enhancements.
    - auto create log options in caller's package
    - rename opt_verbxxx to LOGXXX

--- core/lib/cmt/log.pm
+++ core/lib/cmt/log.pm
@@ -1,37 +1,99 @@
 package cmt::log;
 
 use strict;
-use cmt::util;
+use cmt::util('datetime');
+# use Data::Dumper;
 use Exporter;
-
-# global config:
-our $opt_verbtitle      = 'unknown';
-our $opt_verbtime       = 0;
-our $opt_verbose        = 1;
 
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(_log
                  _sig
                  _sigx
-                 $opt_verbtitle
-                 $opt_verbtime
-                 $opt_verbose
                  );
 
+our $opt_fastlog    = 1;
+
+sub has {
+    my ($pkg, $nam) = @_;
+    my $s = 'use strict; package '.$pkg.'; \$'.$nam;
+          # 'use strict; \$'.$pkg.'::'.$nam;
+    eval $s
+}
+
+sub addmissing {
+    my ($pkg, $nam, $val) = @_;
+    my $ref = has($pkg, $nam);
+    unless (defined $ref) {
+        my $s = 'use strict; package '.$pkg.'; our $'.$nam.' = undef';
+        eval $s;
+        die "can't declare \$$pkg\::$nam: $@" if $@;
+        $ref = has($pkg, $nam)
+            or die "failed to define \$$pkg\::$nam";
+    }
+    $$ref = $val;
+}
+
+sub import {
+    my ($level) = splice @_, 1, 1;
+    my ($pkg) = caller(0);
+
+    addmissing $pkg, 'LOGNAME',     'Who?';
+    addmissing $pkg, 'LOGLEVEL',    1;
+    addmissing $pkg, 'LOGTIME',     0;
+
+    __PACKAGE__->export_to_level(1, @_);
+
+    for (my $i = 0; $i <= $level; $i++) {
+        for (qw(_log _sig)) {
+            my $s = "sub $pkg\::$_$i { \$$pkg\::LOGLEVEL >= $i && &$_ }; 1\n";
+            eval $s or die "can't import level $i: $@";
+        }
+    }
+}
+
+sub _findopt {
+    my $nam = shift;
+    my $c = 1 + shift;
+    while (1) {
+        my ($pkg) = caller($c++) or last;
+        my $ref = has($pkg, $nam);
+        return $ref if ref $ref;
+    }
+    undef # not find in packages in call stack.
+}
+
+# vec cache by call-stack
+# TODO: now using caller(1).package as hash of call-stack
+#       for common scenario, there are not too many log providers,
+#       (typically 2 at most), thus whatever call-stack is,
+#       the results of _findopt are same.
+my %_CS_VEC;
+sub _vec {
+    my ($pkg) = caller(1);
+    my $vec = $_CS_VEC{$pkg};
+    unless (defined $vec) {
+        my $l   = _findopt('LOGLEVEL',  1);
+        my $n   = _findopt('LOGNAME',   1);
+        my $t   = _findopt('LOGTIME',   1);
+        $vec = [ $l, $n, $t ];
+        $_CS_VEC{$pkg} = $vec if $opt_fastlog;
+    }
+    $vec
+}
+
 sub _log {
-    return if $opt_verbose < 1;
-    print STDERR datetime.' ' if $opt_verbtime;
-    print STDERR "[$opt_verbtitle] ", @_, "\n";
+    my $V = _vec; my (undef, $n, $t) = @$V;
+    print STDERR datetime.' ' if $$t;
+    print STDERR "[$$n] ", @_, "\n";
 }
 
 sub _sig {
-    return if $opt_verbose < 1; my $cls = shift; local $_ = join('', @_);
+    my $cls = shift; local $_ = join('', @_);
     return unless -t STDERR or s/\n$//s;
     printf STDERR "[%4s] %-72s".(-t STDERR ? "\r" : "\n"), $cls, $_;
 }
 
 sub _sigx {
-    return if $opt_verbose < 1;
     print STDERR (-t STDERR ? '' : "\n"), '    err: ', @_, "\n";
 }
 
