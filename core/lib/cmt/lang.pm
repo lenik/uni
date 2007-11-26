6c83bab604a916350470469cb28d49a21435ed38
Lenik <pub.lenik@bodz.net>
2007-11-26 05:10:08 +0000

    [perl cmt] cmt::lang: _def(VAR, INIT) used to remove warnings

--- core/lib/cmt/lang.pm
+++ core/lib/cmt/lang.pm
@@ -5,12 +5,15 @@ use Exporter;
 
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(_a
+                 _def
                  _or
                  );
 
 # de-alias
-sub _a      { shift }
+sub _a          { shift }
 
-sub _or     { for (@_) { return $_ if defined $_ } undef }
+sub _def(\$$)   { my $v = shift; $$v = shift unless defined $$v }
+
+sub _or         { for (@_) { return $_ if defined $_ } undef }
 
 1
