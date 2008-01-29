952b59614d4656f470d7bac67b2014b75aa17738
Lenik <pub.lenik@bodz.net>
2008-01-29 16:13:43 +0000

    [-mix] Applied the template for alias, mp3ltrim, odx
    no.bat: get filenames from stdin, for copy/paste.

--- core/abc.d/bin/no.bat
+++ core/abc.d/bin/no.bat
@@ -3,17 +3,34 @@
     setlocal
     call findabc npp .
 
-    set filelist=
-:next
-    if "%~1"=="" goto eol
+    if "%~1"=="--help" goto help
+    if "%~1"=="-h" goto help
 
-    rem file may contains wildchars
-    for %%i in ("%~1") do (
-        set filelist=!filelist! "%%~dpnxi"
+    set filelist=
+    if "%~1"=="-" (
+        set _enter=1
+        shift
     )
 
-    shift
+  :next
+    set _f=
+    if "%_enter%"=="1" (
+        set /p _f=File:
+    ) else (
+        set _f=%~1
+        shift
+    )
+    if "%_f%"=="" goto eol
+    rem file may contains wildchars
+    for %%i in ("%_f%") do (
+        set filelist=!filelist! "%%~dpnxi"
+    )
     goto next
-:eol
+  :eol
 
     start %_home%\notepad++ %filelist%
+    exit /b
+
+:help
+    echo no [- ^| FILELIST]
+    exit /b
