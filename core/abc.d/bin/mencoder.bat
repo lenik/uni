e67e2dca27f8ff0b0abf7081d2292588115bcc38
Lenik <pub.lenik@bodz.net>
2008-02-15 13:59:12 +0000

    [lapiota] Add support for config profiles

--- core/abc.d/bin/mencoder.bat
+++ core/abc.d/bin/mencoder.bat
@@ -19,9 +19,54 @@
     exit /b 1
 
 :_implicit
-    %exec% "%_home%\%name%" %*
-    exit /b
+    set _program=%_home%\%name%
+    goto prep1
 
 :_bin
-    %exec% "%_home%\%bindir%\%name%" %*
+    set _program=%_home%\%bindir%\%name%
+    goto prep1
+
+:prep1
+    if "%~1"=="" (
+        echo mencoder MAJORTYPE [PROFILE [MORE-OPTIONS]]
+        exit /b 1
+    )
+
+    call findabc -r /etc/conf.d mencoder
+    set _conf=%_home%
+    for %%i in ("%_conf%\%~1.*") do (
+        set _majorinc=%%~nxi
+        set _majorext=%%~xi
+    )
+    if "%_majorinc%"=="" (
+        echo major type "%~1%" isn't supported.
+        exit /b 1
+    )
+
+    set _args=-include "%_conf%\%_majorinc%"
+    shift
+
+    if not "%~1"=="" (
+        set _args=%_args% -profile "%~1"
+        shift
+    )
+
+    if exist "%~1" (
+        set _origbase=%~dpn1
+        set _args=%_args% -o "%~dpn1%_majorext%"
+    )
+
+    if exist "%_origbase%.edl" (
+        set _args=%_args% -edl "%_origbase%.edl"
+    )
+
+:prep2
+    if "%~1"=="" goto exec
+    set _args=%_args% %1
+    shift
+    goto prep2
+
+:exec
+    echo %exec% "%_program%" %_args%
+    %exec% "%_program%" %_args%
     exit /b
