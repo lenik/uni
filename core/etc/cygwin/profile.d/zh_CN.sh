ac6158e7953b5380aebdad27e8d644cb7d3db2b7
Lenik <pub.lenik@bodz.net>
2008-09-27 23:45:25 +0000

    zh support.

--- core/etc/cygwin/profile.d/zh_CN.sh
+++ core/etc/cygwin/profile.d/zh_CN.sh
@@ -1,6 +1,15 @@
 
-# alias ls='ls --'
-# alias dir='dir -N --color'
-
 export LANG=zh_CN.GBK
+export LC_ALL=zh_CN.GBK
+export LC_CTYPE=zh_CN.GBK
+
+# export XMODIFIERS='@im=Chinput'
+# e\stty cs8-istrip
+# stty pass8
+
 export OUTPUT_CHARSET=GBK
+
+export LESSCHARSET=latin1
+
+alias dir='dir -N --color'
+alias ls='ls --show-control-chars'
