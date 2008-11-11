b3d8b726ec777f99a5bc60495559aa28dcaa1acd
Lenik <pub.lenik@bodz.net>
2008-11-11 12:19:08 +0000

    [lenik] Add more libs to makew

--- lenik/bin/makew.bat
+++ lenik/bin/makew.bat
@@ -5,6 +5,6 @@
     call vc9
 
     rem  /DCMDW
-    set CC=CL /nologo /W4 atls.lib ole32.lib uuid.lib shell32.lib
+    set CC=CL /nologo /W4 kernel32.lib user32.lib shell32.lib atls.lib ole32.lib uuid.lib
 
     make -e %*
