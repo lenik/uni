60eebd141c119d2803c7112bf683dde9a71c656c
Lenik <pub.lenik@bodz.net>
2010-08-18 07:37:41 +0000

    init xjl- packages

---
+++ uni/xjl-coolgpg/a.bat
@@ -0,0 +1,10 @@
+Key-Type: RSA
+Key-Length: 1024
+Name-Real: Example
+Name-Email: noname@example.com
+Name-Comment: For test purpose
+Expire-Date: 0
+Passphrase: s3cret
+%pubring a.pub
+%secring a.sec
+%commit
