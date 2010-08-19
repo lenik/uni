689e741d5b5e2750470bb807f26a66be09fb0d1f
Lenik <pub.lenik@bodz.net>
2010-08-19 01:03:26 +0000

    Transferred XT-utils to xjl-coolz, -coolutil, -coolhex.

---
+++ uni/xjl-coolbash/xjl-aliases.sh
@@ -0,0 +1,13 @@
+
+alias cd..='cd ..'
+alias cd...='cd ../..'
+alias cd....='cd ../../..'
+
+alias df='df -h'
+
+alias l='ls -FNoh --color'
+alias ll='ls -FANo --color'
+
+alias md='mkdir'
+
+alias rd='rmdir'
