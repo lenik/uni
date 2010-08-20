ea01956e177a1b01b54cdb34205ff59637ed62a1
Lenik <pub.lenik@bodz.net>
2010-08-20 15:28:36 +0000

    Using aliases.d, add more aliases.

---
+++ uni/xjl-coolbash/aliases.d/shellbasic.sh
@@ -0,0 +1,15 @@
+
+alias @='gnome-open'
+
+alias cd..='cd ..'
+alias cd...='cd ../..'
+alias cd....='cd ../../..'
+alias md='mkdir'
+alias rd='rmdir'
+
+alias d='ls  -oF  -N --color'
+alias l='ls  -aoF -N --color'
+alias ll='ls -alF -N --color'
+
+alias df='df -h'
+
