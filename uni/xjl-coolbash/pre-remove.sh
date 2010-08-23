0ddb0e96d8a91c6b6b81ea1fa84a480df70fdaf2
Lenik <pub.lenik@bodz.net>
2010-08-23 00:31:03 +0000

    add eachdir, tourl. and clean .bashrc when uninstall

---
+++ uni/xjl-coolbash/pre-remove.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sysconfdir=$1
+if [ -z "$sysconfdir" ]; then
+    echo sysconfdir is not specified.
+    exit 1
+fi
+
+tmpout=/tmp/tmp.bashrc.$$.$RANDOM
+grep -v "\. $sysconfdir/bash_aliases\b" $sysconfdir/bash.bashrc >$tmpout
+if ! cmp -s $sysconfdir/bash.bashrc $tmpout; then
+    echo Uninstall bash_aliases feature from bashrc.
+    cp -f $tmpout $sysconfdir/bash.bashrc
+fi
+
+exit 0
