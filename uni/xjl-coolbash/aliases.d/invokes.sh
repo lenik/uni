54068ba2e9f0e239acc9b9fe986e621374c5f566
Lenik <pub.lenik@bodz.net>
2010-08-25 07:42:04 +0000

    config sudoers to keep HIST* env vars.
    add orig() to suppress alias.

--- uni/xjl-coolbash/aliases.d/invokes.sh
+++ uni/xjl-coolbash/aliases.d/invokes.sh
@@ -1,2 +1,16 @@
 
 alias @='gnome-open'
+
+function orig() {
+    local cmd
+    if [ $# = 0 ]; then
+        echo orig COMMAND ARGUMENTS...
+        return 1
+    fi
+    if ! cmd=`which "$1"`; then
+        echo "Bad command: $1"
+        return 1
+    fi
+    shift
+    "$cmd" "$@"
+}
