8587f0ae3a8e3f89b5bb23c17b23f41f55a5e841
Lenik <pub.lenik@bodz.net>
2010-09-01 03:39:23 +0000

    add ver-refresh alias.

--- uni/xjl-cooldeb/cooldeb.sh
+++ uni/xjl-cooldeb/cooldeb.sh
@@ -3,3 +3,4 @@ alias sagi="sudo apt-get install"
 
 alias ver-on='_base=${PWD##*/}; cd ..; cd `dir-ver "$_base"`'
 alias ver-off='_base=${PWD##*/}; cd ..; cd `dir-nover "$_base"`'
+alias ver-refresh='ver-off; ver-on'
