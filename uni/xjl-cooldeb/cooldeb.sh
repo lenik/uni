ddf62a383b1bd7d2248088dc4ba4cb2dfe872372
Lenik <pub.lenik@bodz.net>
2010-09-07 11:29:35 +0000

    default with --createorig

--- uni/xjl-cooldeb/cooldeb.sh
+++ uni/xjl-cooldeb/cooldeb.sh
@@ -5,4 +5,4 @@ alias ver-on='_base=${PWD##*/}; cd ..; cd `dir-ver "$_base"`'
 alias ver-off='_base=${PWD##*/}; cd ..; cd `dir-nover "$_base"`'
 alias ver-refresh='ver-off; ver-on'
 
-alias dh_makever='. dh_make-autover'
+alias dh_makever='. dh_make-autover --createorig'
