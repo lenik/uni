d00d3cb6a1a568f32eda5e3b01b87bd88b4b7233
Lenik <pub.lenik@bodz.net>
2010-09-07 11:33:33 +0000

    add sau, sagu alias.

--- uni/xjl-cooldeb/cooldeb.sh
+++ uni/xjl-cooldeb/cooldeb.sh
@@ -1,5 +1,7 @@
 
-alias sagi="sudo apt-get install"
+alias sagi='sudo apt-get install'
+alias sagu='sudo apt-get remove'
+alias sau='sudo apt-get update'
 
 alias ver-on='_base=${PWD##*/}; cd ..; cd `dir-ver "$_base"`'
 alias ver-off='_base=${PWD##*/}; cd ..; cd `dir-nover "$_base"`'
