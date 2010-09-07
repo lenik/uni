9d4f6a8fa51d93276863b33100f2eec3152fa708
Lenik <pub.lenik@bodz.net>
2010-09-07 10:06:26 +0000

    initial deb-redist.

--- uni/xjl-cooldeb/cooldeb.sh
+++ uni/xjl-cooldeb/cooldeb.sh
@@ -4,3 +4,5 @@ alias sagi="sudo apt-get install"
 alias ver-on='_base=${PWD##*/}; cd ..; cd `dir-ver "$_base"`'
 alias ver-off='_base=${PWD##*/}; cd ..; cd `dir-nover "$_base"`'
 alias ver-refresh='ver-off; ver-on'
+
+alias dh_makever='. dh_make-autover'
