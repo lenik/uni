00a2b81d3d593da645e242bb1ad7ba1d891864a7
Lenik <pub.lenik@bodz.net>
2010-09-14 12:28:48 +0000

    add install-truecrypt.sh on the fly. add fstab/passwd-install.tc which add entries by incremental. add test scripts.

---
+++ uni/xjl-cooltruecrypt/install-truecrypt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+dl="http://www.truecrypt.org/download/truecrypt-7.0a-linux-console-x86.tar.gz"
+dl_file=`wgetc $dl`
+single_file=`tar tzf $dl_file`
+
+tar xvf $dl_file $single_file
+mv $single_file $1
