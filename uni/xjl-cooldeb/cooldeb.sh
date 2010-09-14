556b923ea2887fc31ba2c87f902dbf8a396efd38
Lenik <pub.lenik@bodz.net>
2010-09-14 12:33:53 +0000

    cooldeb.sh: sagi/sau allow unauth.
    add newdeb to quickstart.
    
    deb-redist supports .env file.

--- uni/xjl-cooldeb/cooldeb.sh
+++ uni/xjl-cooldeb/cooldeb.sh
@@ -1,10 +1,12 @@
 
-alias sagi='sudo apt-get install'
+alias sagi='sudo apt-get install --allow-unauthenticated'
 alias sagu='sudo apt-get remove'
-alias sau='sudo apt-get update; sudo apt-get upgrade'
+alias sau='sudo apt-get update; sudo apt-get dist-upgrade --allow-unauthenticated'
 
 alias ver-on='_base=${PWD##*/}; cd ..; cd `dir-ver "$_base"`'
 alias ver-off='_base=${PWD##*/}; cd ..; cd `dir-nover "$_base"`'
 alias ver-refresh='ver-off; ver-on'
 
 alias dh_makever='. dh_make-autover --createorig'
+
+alias newdeb='newdeb -aps'
