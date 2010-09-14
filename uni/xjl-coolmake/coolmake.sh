5e42480d491c25506601cb9d698952651cbff0b5
Lenik <pub.lenik@bodz.net>
2010-09-14 12:47:17 +0000

    tree -a

--- uni/xjl-coolmake/coolmake.sh
+++ uni/xjl-coolmake/coolmake.sh
@@ -1,4 +1,6 @@
 alias smi="sudo make install"
 alias smu="sudo make uninstall"
 
+alias reinst='reconf && makelook && sudo make install && unconf'
+
 # alias check="make check distcheck installcheck"
