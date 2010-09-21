4b69c35d6099ff51c670c5c570c2d0cfaf44b71e
Lenik <pub.lenik@bodz.net>
2010-09-21 08:45:55 +0000

    add dh_package_field for analyse depends fields.

--- uni/xjl-cooldeb/cooldeb.sh
+++ uni/xjl-cooldeb/cooldeb.sh
@@ -1,7 +1,9 @@
 
 alias sagi='sudo apt-get install --allow-unauthenticated'
 alias sagu='sudo apt-get remove'
+alias sagp='sudo apt-get purge'
 alias sagr='sudo apt-get-reinstall2'
+alias sagc='sudo apt-get autoclean'
 alias sau='sudo apt-get update; sudo apt-get dist-upgrade --allow-unauthenticated'
 
 alias ver-on='_base=${PWD##*/}; cd ..; cd `dir-ver "$_base"`'
