c4f4d69564080f8243ce3677a6ec7a20cd3ecb90
Lenik <pub.lenik@bodz.net>
2010-10-06 01:17:19 +0000

    add alias mlk=makelook

--- uni/xjl-coolmake/coolmake.sh
+++ uni/xjl-coolmake/coolmake.sh
@@ -1,6 +1,7 @@
 alias smi="sudo make install"
 alias smu="sudo make uninstall"
 
+alias mlk='makelook'
 alias reinst='reconf && makelook && sudo make install && unconf'
 
 # alias check="make check distcheck installcheck"
