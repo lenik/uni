4c8f49263f5b420f63fd50d08f633641b4d2a864
Lenik <pub.lenik@bodz.net>
2010-10-08 15:35:00 +0000

    add update releated aliases

--- uni/xjl-coolsvn/aliases.d/coolsvn.sh
+++ uni/xjl-coolsvn/aliases.d/coolsvn.sh
@@ -7,3 +7,8 @@ alias st='svn st'
 alias ci='svn ci'
 alias di='svn di'
 alias sraw='svn resolve --accept working'
+alias upraw='svn update; svn resolve --accept working'
+
+alias up='svn update'
+alias upim='svn update --set-depth immediates'
+alias upin='svn update --set-depth infinity'
