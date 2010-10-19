ed92bf24f81b3f739d8dfa1cda687673aa661e47
Lenik <pub.lenik@bodz.net>
2010-10-19 10:08:11 +0000

    separate -aptutils from -cooldeb.

---
+++ uni/xjl-aptutils/aptutils.sh
@@ -0,0 +1,7 @@
+alias as='aptitude search'
+alias sagi='sudo apt-get install --allow-unauthenticated'
+alias sagu='sudo apt-get remove'
+alias sagp='sudo apt-get purge'
+alias sagr='sudo apt-get-reinstall2'
+alias sagc='sudo apt-get autoclean'
+alias sau='sudo apt-get update; sudo apt-get dist-upgrade --allow-unauthenticated'
