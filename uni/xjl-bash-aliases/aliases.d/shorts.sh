8279d2fd5d45f7c4951c830c491e3bc2fed59369
Lenik <pub.lenik@bodz.net>
2010-10-19 04:16:05 +0000

    moved aliases.d/* to -coolbash, -bash-aliases do only bash_aliases.d preparation

--- uni/xjl-bash-aliases/aliases.d/shorts.sh
+++
@@ -1,4 +0,0 @@
-alias ps='COLUMNS=1000 ps fx'
-alias up='svn update --ignore-externals'
-
-alias sci='scilab -nwni'
--- uni/xjl-bash-aliases/aliases.d/stats.sh
+++
@@ -1,9 +0,0 @@
-alias hist="HISTTIMEFORMAT= history \
-    | awk '{ \$1=\"\"; print \$0 }' \
-    | sort | uniq -c | sort -n | grep -v '^ *[1-9] '"
-alias hist1="HISTTIMEFORMAT= history \
-    | awk '{ print \$2 }' \
-    | sort | uniq -c | sort -n | grep -v '^ *[1-9] '"
-alias hist2="HISTTIMEFORMAT= history \
-    | awk '{ print \$2, \$3 }' \
-    | sort | uniq -c | sort -n | grep -v '^ *[1-9] '"
