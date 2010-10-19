185bd1d703c08b6e9f121e36d7ef17bdf96e649f
Lenik <pub.lenik@bodz.net>
2010-10-19 11:27:59 +0000

    move dwhich and apt* aliaes to -aptutils.

--- uni/xjl-cooldeb/cooldeb.sh
+++ uni/xjl-cooldeb/cooldeb.sh
@@ -1,12 +1,3 @@
-
-alias as='aptitude search'
-alias sagi='sudo apt-get install --allow-unauthenticated'
-alias sagu='sudo apt-get remove'
-alias sagp='sudo apt-get purge'
-alias sagr='sudo apt-get-reinstall2'
-alias sagc='sudo apt-get autoclean'
-alias sau='sudo apt-get update; sudo apt-get dist-upgrade --allow-unauthenticated'
-
 alias ver-on='_base=${PWD##*/}; cd ..; cd `dir-ver "$_base"`'
 alias ver-off='_base=${PWD##*/}; cd ..; cd `dir-nover "$_base"`'
 alias ver-refresh='ver-off; ver-on'
