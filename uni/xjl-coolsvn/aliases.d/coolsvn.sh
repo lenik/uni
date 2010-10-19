30b77e34c9fbdadfc004ceba0f5c9fade5e07846
Lenik <pub.lenik@bodz.net>
2010-10-19 10:35:40 +0000

    move x11-related tools to -svnguis

--- uni/xjl-coolsvn/aliases.d/coolsvn.sh
+++ uni/xjl-coolsvn/aliases.d/coolsvn.sh
@@ -1,11 +1,10 @@
-
 alias svn=coolsvn
 
 alias grep-svn='grep --exclude-dir .svn'
 
 alias st='svn st'
 alias ci='svn ci'
-alias di='svn di --diff-cmd meld'
+alias di='svn di'
 
 alias sraw='svn resolve --accept working'
 alias upraw='svn update; svn resolve --accept working'
