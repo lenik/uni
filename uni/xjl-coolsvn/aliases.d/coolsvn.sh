3e8428d2ce0259076b7f123139f689024a7b5038
Lenik <pub.lenik@bodz.net>
2010-11-18 01:38:54 +0000

    split coolsvn -> svn-commit-autover and svn-unadd. new svn-log to show log with highlight and pager.

--- uni/xjl-coolsvn/aliases.d/coolsvn.sh
+++ uni/xjl-coolsvn/aliases.d/coolsvn.sh
@@ -1,14 +1,14 @@
-alias svn=coolsvn
-
 alias grep-svn='grep --exclude-dir .svn'
 
-alias st='svn st'
-alias ci='svn ci'
-alias di='svn di'
+alias unadd='svn-unadd'
+alias    ci='svn-commit-autover'
+alias    di='svn di'
+alias    st='svn st'
+alias   log='svn-log'
 
-alias sraw='svn resolve --accept working'
+alias    up='svn update'
+alias  upim='svn update --set-depth immediates'
+alias  upin='svn update --set-depth infinity'
+
+alias  sraw='svn resolve --accept working'
 alias upraw='svn update; svn resolve --accept working'
-
-alias up='svn update'
-alias upim='svn update --set-depth immediates'
-alias upin='svn update --set-depth infinity'
