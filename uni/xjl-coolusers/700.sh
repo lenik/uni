be2bbad61e38ebf6950c49add8aa8f6488ab4cd7
Lenik <pub.lenik@bodz.net>
2010-11-28 09:34:46 +0000

    add wine user

--- uni/xjl-coolusers/700.sh
+++ uni/xjl-coolusers/700.sh
@@ -92,6 +92,8 @@ add_user 720 appserv  dev  "App Server"          /home/appserv
 
 add_user 730 scm      dev  "VCS daemon"          /none /bin/false
 
+add_user 740 wine     dev  "WINE player"         /home/wine /bin/bash
+
 # user 800-999
 
 add_user 900 play     play "Player"
