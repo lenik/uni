b89bae081d98cd7373a63d34c45869572ab04a1d
Lenik <pub.lenik@bodz.net>
2010-11-29 08:18:12 +0000

    set default grep as grep-svn.

--- uni/xjl-coolsvn/aliases.d/coolsvn.sh
+++ uni/xjl-coolsvn/aliases.d/coolsvn.sh
@@ -1,10 +1,12 @@
 alias grep-svn='grep --exclude-dir .svn'
+alias  grep='grep-svn'
 
 alias unadd='svn-unadd'
 alias    ci='svn-commit-autover'
 alias    di='svn di'
 alias    st='svn st'
 alias   log='svn-log'
+alias   siu='svn-ignore-unversioned'
 
 alias    up='svn update'
 alias  upim='svn update --set-depth immediates'
