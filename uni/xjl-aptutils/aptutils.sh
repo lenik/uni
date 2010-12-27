3afe476ce596a63cd55612e18f5e46e0bbb8c7bd
Lenik <pub.lenik@bodz.net>
2010-12-27 14:09:31 +0000

    change alias as -> search add alias show.

--- uni/xjl-aptutils/aptutils.sh
+++ uni/xjl-aptutils/aptutils.sh
@@ -1,4 +1,6 @@
-alias as='aptitude search'
+alias search='postproc xxcat \; aptitude search'
+alias show='postproc xxcat \; aptitude show'
+
 alias sagi='sudo apt-get install --allow-unauthenticated'
 alias sagu='sudo apt-get remove'
 alias sagp='sudo apt-get purge'
