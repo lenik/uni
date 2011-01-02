3e53aad18fb90d1ecad4e13f55c4ad6e3190fb41
Lenik <pub.lenik@bodz.net>
2011-01-02 14:49:58 +0000

    add TP='cd-debian-package' alias and completion.

--- uni/package/cooldeb/cooldeb.sh
+++ uni/package/cooldeb/cooldeb.sh
@@ -1,3 +1,5 @@
+# vim: set filetype=sh :
+
 alias ver-on='_base=${PWD##*/}; cd ..; cd `dir-ver "$_base"`'
 alias ver-off='_base=${PWD##*/}; cd ..; cd `dir-nover "$_base"`'
 alias ver-refresh='ver-off; ver-on'
@@ -10,7 +12,22 @@ alias dcon='vicontrol'
 
 alias updist='svn-update-and-deb-redist'
 
-alias epc='edit-patch -c'
 alias depc='dpatch-edit-patch -c'
 
 alias eachdeb='eachdir -rfdebian/control'
+
+function cd-debian-package() {
+    local dir
+    local base
+    while read dir; do
+        base="${dir%/}"
+        base="${base##*/}"
+        if [ "$base" = "$1" ]; then
+            cd "$dir"
+            return 0
+        fi
+    done < <(eachdir -rlf debian/control)
+    echo "No such package: $1" >&2
+    return 1
+}
+alias TP='cd-debian-package'
