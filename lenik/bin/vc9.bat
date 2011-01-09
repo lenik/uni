64b7f3f61d9d7dc02b0ae10e42a67ff57fed9021
Lenik <pub.lenik@bodz.net>
2011-01-09 18:21:21 +0800

    [lenik] Updated vc9.bat.

--- lenik/bin/vc9.bat
+++ lenik/bin/vc9.bat
@@ -1,11 +1,16 @@
 @echo off
 
+    REM TODO: put in config.bat
+    REM ...
+    set             PATH=C:\lam\nt.util\lib;%PATH%
+
+
     call findabc -r C:\lam\nt.util\abc.d mssdk
 
     rem Platform SDK
+    set             PATH=%_home%\bin;%PATH%
     set          INCLUDE=%_home%\include;%INCLUDE%
     set              LIB=%_home%\lib;%LIB%
-    set             PATH=%_home%\bin;%PATH%
 
     call findabc -r C:\lam\nt.util\abc.d msvs-9
 
@@ -15,11 +20,11 @@
     set     VSINSTALLDIR=%_home%
 
     rem Framework SDK
+    set             PATH=%_FRAMEWORK%\v3.5;%_FRAMEWORK%\v2.0.50727;%PATH%
     set       _FRAMEWORK=C:\WINDOWS\Microsoft.NET\Framework
     set          LIBPATH=%_FRAMEWORK%\v3.5;%_FRAMEWORK%\v2.0.50727;%_home%\VC\ATLMFC\LIB;%_home%\VC\LIB;
-    set             PATH=%_FRAMEWORK%\v3.5;%_FRAMEWORK%\v2.0.50727;%PATH%
 
     rem VC
+    set             PATH=%_home%\Common7\IDE;%_home%\VC\BIN;%_home%\Common7\Tools;%_home%\VC\VCPackages;%PATH%
     set          INCLUDE=%_home%\VC\ATLMFC\INCLUDE;%_home%\VC\INCLUDE;%INCLUDE%
     set              LIB=%_home%\VC\ATLMFC\LIB;%_home%\VC\LIB;%LIB%
-    set             PATH=%_home%\Common7\IDE;%_home%\VC\BIN;%_home%\Common7\Tools;%_home%\VC\VCPackages;%PATH%
