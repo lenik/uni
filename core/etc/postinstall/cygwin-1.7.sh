f22adb123c423ee3bd469ff809193702dc2c3e56
Lenik <pub.lenik@bodz.net>
2011-01-09 18:48:11 +0800

    [lapiota] Updated cygwin post-installs.

--- core/etc/postinstall/cygwin-1.7.sh
+++ core/etc/postinstall/cygwin-1.7.sh
@@ -1,13 +1,73 @@
 #!/bin/bash
+# Cygwin/Lapiota post-install init
+    echo "Current cygwin installation version: "
+    uname -srv
+    echo
 
-ln -nsf $LAM_KALA/xt        /xt
-ln -nsf /xt/etc/profile.d   /etc/profile.d/xt
-ln -nsf /mnt/d/             /media/cpan
+    if [ -d /cygdrive/c -o ! -d /mnt/c ]; then
+        echo "cygdrive path prefix should be set to /mnt"
+        # show user/system cygdrive path prefix.
+        mount -p
+        exit 1
+    fi
 
-if [ -d /usr/lib/perl5/site_perl/5.10 ]; then
-    mv /usr/lib/perl5/site_perl/5.10 /usr/lib/perl5/site_perl/5.10.orig
-fi
-ln -nsf $LAM_KALA/usr/lib/perl5/site /usr/lib/perl5/site_perl/5.10
+# helper functions
+    function contains() {
+        local files=("$@")
+        # echo "Files: ${files[@]}"
+        [ ${#files} != 0 ]
+    }
 
-ln -nsf $LAM_KALA/etc/cygwin/etc/*       /etc
-ln -nsf $LAM_KALA/etc/profile.d          /etc/profile.d/cygwin
+# base
+    shopt -s nullglob
+    mkdir -p /mirror /opt /media
+    mkdir -p /var/cron/tabs
+    CYGROOT=`cygpath -am /`
+    LAM_SYS=`cygpath -au "${CYGROOT%/*}"`
+
+    mkgroup -l >/etc/group
+    mkpasswd -l >/etc/passwd
+
+    ln -nsf /mnt/c/lam          /lam
+    ln -nsf $LAM_KALA           /lapiota
+    ln -nsf $LAM_KALA/xt        /xt
+    ln -nsf /xt/etc/profile.d   /etc/profile.d/xt
+
+    ln -nsf $LAM_KALA/etc/cygwin/*      /etc
+    ln -nsf $LAM_KALA/etc/profile.d     /etc/profile.d/cygwin
+
+    ln -nsf /mnt/b              /test
+    rm -fR /tmp
+    ln -nsf "$TEMP"             /tmp
+
+# using cache of updates
+    if [ -d /usr/lib/perl5/site_perl/5.10 ]; then
+        mv /usr/lib/perl5/site_perl/5.10 /usr/lib/perl5/site_perl/5.10.orig
+    fi
+    for site in $LAM_KALA/usr/lib/perl5/site $LAM_SYS/lib/perl5/site; do
+        if [ -d "$site" ]; then
+            echo "Redirect perl5-site -> $site"
+            ln -nsf "$site" /usr/lib/perl5/site_perl/5.10
+            break
+        fi
+    done
+
+# per user configs
+    while IFS=: read name pass uid gid info home shellcmd; do
+        if contains "$home/etc/cygwin"/*; then
+            echo "install etc/: " ~/etc/cygwin/*
+            ln -nsf ~/etc/cygwin/* /etc
+        fi
+        if [ -f "$home/etc/crontab" ]; then
+            echo "Install crontab /var/cron/tabs/$USERNAME"
+            ln -nsf ~/etc/crontab /var/cron/tabs/$USERNAME
+        fi
+    done </etc/passwd
+
+# patches
+    xpatch -c4 /bin/bash.exe <(echo -e "56c56\n< 03000080\n---\n> 02000080") /bin/bashw.exe
+    xpatch -c4 /bin/perl.exe <(echo -e "56c56\n< 03000000\n---\n> 02000000") /bin/perlw.exe
+
+# system wide configs
+    cygserver-config
+    ssh-host-config
