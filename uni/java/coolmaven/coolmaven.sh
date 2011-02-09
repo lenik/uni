c5832ebb5e919f8622085e135af7cafefb365bce
Lenik <pub.lenik@bodz.net>
2011-02-09 14:26:22 +0800

    New shortcuts MPK/MIN/MDP for force-build.

--- uni/java/coolmaven/coolmaven.sh
+++ uni/java/coolmaven/coolmaven.sh
@@ -3,6 +3,11 @@ alias mpk='mvn package'
 alias mte='mvn test'
 alias min='mvn install'
 alias mdp='mvn deploy'
+
+alias MPK='mvn package -fn -Dmavne.test.skip=true'
+alias MIN='mvn install -fn -Dmaven.test.skip=true'
+alias MDP='mvn deploy -fn -Dmaven.test.skip=true'
+
 alias 'mr-'='cd.x mvn release:prepare'
 alias mr+='cd.x mvn release:perform'
 alias mr0='cd.x mvn release:rollback'
