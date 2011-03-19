5e811e33607fb001eb9f03c34c0b9379dd8c0480
Lenik <pub.lenik@bodz.net>
2011-03-19 17:35:38 +0800

    Add m2release (orig. M+)

--- uni/java/coolmaven/coolmaven.sh
+++ uni/java/coolmaven/coolmaven.sh
@@ -8,6 +8,7 @@ alias MPK='mvn package -fn -Dmavne.test.skip=true'
 alias MIN='mvn install -fn -Dmaven.test.skip=true'
 alias MDP='mvn deploy -fn -Dmaven.test.skip=true'
 
+alias M+='m2release'
 alias 'mr-'='cd.x mvn release:prepare'
 alias mr+='cd.x mvn release:perform'
 alias mr0='cd.x mvn release:rollback'
