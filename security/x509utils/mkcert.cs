3d19653ee5b9b3e441d8d0dc7c3125a3501cdf66
Lenik <pub.lenik@bodz.net>
2012-02-15 00:52:31 +0800

    Added support of multiple domain names in CSRs.

--- security/x509utils/mkcert.cs
+++ security/x509utils/mkcert.cs
@@ -26,38 +26,49 @@ function passphrase() {
 
 function csr-config() {
     local private_key="$1"
-
-    cat <<EOT
-[ca]
-default_ca              = CA_default
-
-[CA_default]
-x509_extensions         = root_ca_extensions
-
-[req]
-default_bits            = 4096
-default_keyfile         = $private_key
-distinguished_name      = req_distinguished_name
-attributes              = req_attributes
-prompt                  = no
-x509_extensions         = v3_ca
-
-[req_distinguished_name]
-C                       = $DN_C
-ST                      = $DN_ST
-O                       = $DN_O
-OU                      = $DN_OU
-CN                      = $DN_CN
-emailAddress            = $DN_emailAddress
-
-[req_attributes]
-
-[root_ca_extensions]
-basicConstraints        = CA:true
-
-[v3_ca]
-basicConstraints        = CA:true
-EOT
+    echo "[ca]"
+    echo "default_ca              = CA_default"
+    echo
+    echo "[CA_default]"
+    echo "x509_extensions         = root_ca_extensions"
+    echo
+    echo "[req]"
+    echo "default_bits            = 4096"
+    echo "default_keyfile         = $private_key"
+    echo "distinguished_name      = req_distinguished_name"
+    echo "attributes              = req_attributes"
+    echo "prompt                  = no"
+    echo "req_extensions          = v3_req"
+    echo "x509_extensions         = v3_ca"
+    echo
+    echo "[req_distinguished_name]"
+    [ -n "$DN_C" ] && echo "C     = $DN_C"
+    [ -n "$DN_ST" ] && echo "ST   = $DN_ST"
+    [ -n "$DN_O" ] && echo "O     = $DN_O"
+    [ -n "$DN_OU" ] && echo "OU   = $DN_OU"
+    [ -n "$DN_CN" ] && echo "CN   = $DN_CN"
+    [ -n "$DN_emailAddress" ] &&
+        echo "emailAddress        = $DN_emailAddress"
+    echo
+    echo "[req_attributes]"
+    echo
+    echo "[root_ca_extensions]"
+    echo "basicConstraints        = CA:true"
+    echo
+    echo "[v3_ca]"
+    echo "basicConstraints        = CA:true"
+    echo
+    echo "[v3_req]"
+    echo "basicConstraints        = CA:false"
+    echo "keyUsage                = nonRepudiation, digitalSignature, keyEncipherment"
+    echo "subjectAltName          = @alt_names"
+    echo
+    echo "[alt_names]"
+    index=1
+    for domain in "${domains[@]}"; do
+        echo "DNS.$index = $domain"
+        (( index++ ))
+    done
 }
 
 function x509-opts() {
