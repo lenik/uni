fa2d0f4b277b8e2729f66b1421521d1eef8a660a
Lenik <pub.lenik@bodz.net>
2012-04-07 14:42:14 +0800

    Added alias h=man, H=info.

--- utils/coolcmd/coolcmd.sh
+++ utils/coolcmd/coolcmd.sh
@@ -1,8 +1,12 @@
 alias @='gnome-open'
 alias %='substexec'
+
 alias B='. run-bg'
 alias C='LANG=C'
 alias D='. cd-exec'
 alias L='. loop 1'
 alias F='fnotify'
 alias X='xcat'
+
+alias h='man'
+alias H='info'
