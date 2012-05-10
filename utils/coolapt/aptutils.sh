58edfb639805a0a3216fff86e52fb3b45608f768
Lenik <pub.lenik@bodz.net>
2012-05-10 14:08:42 +0800

    Added safi = sudo apt-fast install.

--- utils/coolapt/aptutils.sh
+++ utils/coolapt/aptutils.sh
@@ -1,6 +1,7 @@
 alias search='postproc xxcat \; aptitude search'
 alias show='postproc xxcat \; aptitude show'
 
+alias safi='sudo apt-fast install --allow-unauthenticated'
 alias sagi='sudo apt-get install --allow-unauthenticated'
 alias sagu='sudo apt-get remove'
 alias sagp='sudo apt-get purge'
