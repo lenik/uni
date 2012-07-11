dfc06cd709f416882c186c79f7674a2b57ce92e8
Lenik <pub.lenik@bodz.net>
2012-07-11 21:12:30 +0800

    Fixed filename too long: `wgetc -q ...` to get a downloaded path.

--- toolenh/truecrypt-enh/install-truecrypt.sh
+++ toolenh/truecrypt-enh/install-truecrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 dl="http://www.truecrypt.org/download/truecrypt-7.0a-linux-console-x86.tar.gz"
-dl_file=`wgetc $dl`
+dl_file=`wgetc -q $dl`
 single_file=`tar tzf $dl_file`
 
 tar xvf $dl_file $single_file
