672d9a176377e664fa2ab5f663791dc1dee49e71
Lenik <pub.lenik@bodz.net>
2012-10-24 22:10:17 +0800

    bash-enh: Added bc alias for `bc -l maths.b`.

---
+++ toolenh/bash-enh/aliases.d/maths.b
@@ -0,0 +1,5 @@
+# vim: set filetype=bc :
+
+define pow(base, expo) {
+    return e(l(base) * expo);
+}
--- toolenh/bash-enh/aliases.d/shorts.sh
+++ toolenh/bash-enh/aliases.d/shorts.sh
@@ -1,6 +1,4 @@
 alias mam='man -Pmore'
 alias ps='COLUMNS=1000 ps fx'
 alias rp='rsync -a --progress'
-alias sci='scilab -nwni'
 alias up='svn update --ignore-externals'
-
