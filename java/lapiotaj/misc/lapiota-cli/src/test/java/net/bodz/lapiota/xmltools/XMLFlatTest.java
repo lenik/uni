de605c9b6aa6b04cffe0d12437f52aa87fb1ce7b
Lenik <pub.lenik@bodz.net>
2012-12-21 17:21:58 +0800

    lapiotaj: Refactor/ref: Moved snmjava eclipse utils to bas-c3.

--- java/lapiotaj/misc/lapiota-cli/src/test/java/net/bodz/lapiota/xmltools/XMLFlatTest.java
+++ java/lapiotaj/misc/lapiota-cli/src/test/java/net/bodz/lapiota/xmltools/XMLFlatTest.java
@@ -4,15 +4,15 @@ import java.io.File;
 
 import org.junit.Test;
 
-import net.bodz.bas.snm.eclipse.EclipseProject;
+import net.bodz.bas.c.org.eclipse.EclipseProject;
 
 public class XMLFlatTest {
 
     @Test
     public void test1()
             throws Throwable {
-        EclipseProject proj = EclipseProject.findFromCWD();
-        File file = new File(proj.getBase(), ".classpath");
+        EclipseProject proj = EclipseProject.findBaseDirFromWorkdir();
+        File file = new File(proj.getBaseDir(), ".classpath");
         XMLFlat flat = new XMLFlat();
         flat.execute("-f", file.getPath(), "-s", "//classpathentry", "@path");
     }
