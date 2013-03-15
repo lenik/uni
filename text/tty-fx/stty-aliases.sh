5d861b24537c06339f13906979c8f40aa53fdb25
Lenik <pub.lenik@bodz.net>
2013-03-15 20:04:23 +0800

    tty-fx: Renamed coolansi to tty-fx, and added stty ⚝ aliases.

---
+++ text/tty-fx/stty-aliases.sh
@@ -0,0 +1,25 @@
+# vim: set filetype=sh :
+
+function Eoff() {
+    stty -echo
+}
+
+function Eon() {
+    stty echo
+}
+
+# ^+u269d
+function ⚝() {
+    local _stty=`stty -a`
+    if [ "$_stty" = "${_stty/-echo }" ]; then
+        # echo Disable echo mode.
+        _PS1_ECHO="$PS1"
+        PS1="⚝  "
+        stty -echo
+    else
+        # echo Restore echo mode.
+        PS1="$_PS1_ECHO"
+        _PS1_ECHO=
+        stty echo
+    fi
+}
