d2c9994c5cd9ec4ed4b40b98897ae8afb8f708d2
Lenik <pub.lenik@bodz.net>
2013-12-05 22:16:14 +0800

    libbas-c: log: Instead of perror(), append the error message to the logging message.

--- lib/libbas-c/src/log.c
+++ lib/libbas-c/src/log.c
@@ -1,5 +1,7 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdarg.h>
+#include <stdio.h>                      /* sys_errlist, sys_nerr */
 #include <string.h>
 #include <syslog.h>
 #include <bas/log.h>
@@ -19,14 +21,23 @@ void _log_x(const char *ident, int option, int level,
 }
 
 void _log_x_perror(const char *ident, int option, int level, const char *format, ...) {
+    char msgbuf[1024];
+    char *errmsg = "???";
+    int errlen;
+
+    if (errno >= 0 && errno < sys_nerr)
+        errmsg = sys_errlist[errno];
+    errlen = strlen(errmsg);
+
     va_list ap;
     va_start(ap, format);
-
-    openlog(ident, option, syslog_facility);
-    vsyslog(level, format, ap);
-    closelog();
-
+    vsnprintf(msgbuf, sizeof(msgbuf) - (errlen + 2), format, ap);
     va_end(ap);
 
-    perror("");
+    strcat(msgbuf, ": ");
+    strcat(msgbuf, errmsg);
+
+    openlog(ident, option, syslog_facility);
+    syslog(level, "%s", msgbuf);
+    closelog();
 }
