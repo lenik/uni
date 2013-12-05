c5f82697aefd0cbaaaf644275cc58e56488bd095
Lenik <pub.lenik@bodz.net>
2013-12-05 15:56:19 +0800

    shendun-ef: Implemented the auth data parser.

--- hack/shendun-ef/shendun-efext.c
+++ hack/shendun-ef/shendun-efext.c
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <assert.h>
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -13,10 +14,6 @@
 #include "ywcrypt.h"
 #include "log.h"
 
-#define AUTH_FILE "@cachedir@/auth.dat"
-#define RENEW_PROGRAM "shendun-renew"
-#define FAILURE_MAX 5
-
 /* .yw decoder */
 
 void decode(char *buf, int size) {
@@ -25,53 +22,31 @@ void decode(char *buf, int size) {
 
 /* global gate */
 
+#define AUTH_FILE       "@rundir@/sdefauth"
+#define AUTH_MAC_MAX    99
+#define FAILURE_MAX     5
+#define RENEW_CMD       "./shendun-efpoll.ain -r"
+
 struct auth_db {
-    char mac[16];             /* (128-bit) hash of machine identity */
-    int registered;           /* the mac code is registered somewhere remote. */
-    int acc;                  /* used time accumulator, in seconds. */
-    int alloc;                /* allocated time, in seconds. */
-    int failure;              /* communication failure counter */
+    char mac[AUTH_MAC_MAX + 1];         /* (128-bit) hash of machine identity */
+    long creation;                      /* authenticated start date */
+    long expire;                        /* authenticated expire date */
+    long now;                           /* current time in the auth server */
+    int configs;                        /* re-config counter */
+    int fails;                          /* communication failure counter */
+    int serial;                         /* file serial (update counter) */
 };
 
-static bool parse_auth(char *script, struct auth_db *db) {
-    char *head = script;
-    char *p;
-    char *token;
+static struct auth_db db;
+static time_t mtime;
 
-    while ((p = strtok_eol(head, &head))) {
-        token = readtok(&p);
-        if (token == NULL)              /* empty line */
-            continue;
-        else
-            rtrim(p);
-
-        switch (*token) {
-        case '#':
-            continue;                   /* comment line */
-
-        case 'm':
-            if (streq("mac", token)) {
-                strncpy(db->mac, p, 16);
-                break;
-            }
-            break;
-
-        default:
-            log_err("illegal syntax in auth data: %s %s.", token, p);
-            return false;
-        } /* switch: token */
-    } /* while: strtok_eol */
-    return true;
-}
+static bool parse_auth(char *script, struct auth_db *db);
 
 bool gate(pid_t pid) {
-    static struct auth_db db;
-    static time_t mtime;
-
     struct stat sb;
     if (stat(AUTH_FILE, &sb) != 0) {
         if (errno == ENOENT) {          /* auth file isn't existed */
-            int exit = system(RENEW_PROGRAM);
+            int exit = system(RENEW_CMD);
             if (exit != 0) {
                 log_err("Failed to execute the auth updater.");
                 return false;
@@ -83,11 +58,6 @@ bool gate(pid_t pid) {
     }
 
     if (sb.st_mtime > mtime) {          /* file changed, reload it anyway. */
-        if (! auth_reload())            /* became illegal... */
-            return false;
-
-        stat(AUTH_FILE, &sb);
-
         size_t size;
         char *auth_data = load_file(AUTH_FILE, &size, 0, 1);
 
@@ -108,15 +78,103 @@ bool gate(pid_t pid) {
         free(auth_data);
     }
 
-    if (! db.registered) {
+    if (!strlen(db.mac) || db.expire == 0) {
         log_err("Software isn't registered, yet.");
         return false;
     }
 
-    if (db.acc > db.alloc) {            /* allocated time is used up. */
+    if (db.now > db.expire) {
         log_err("Registration is expired.");
         return false;
     }
 
+    if (db.fails > FAILURE_MAX) {
+        log_err("Too many network failures.");
+        return false;
+    }
+
     return true;
+}
+
+bool parse_auth(char *script, struct auth_db *db) {
+    char *head = script;
+    char *p;
+    char *token;
+    char *delim;
+    char *arg;
+
+    while ((p = strtok_eol(head, &head))) {
+        token = readtok(&p);
+        if (token == NULL)              /* empty line */
+            continue;
+        else
+            rtrim(p);
+        if (*token == '#')
+            continue;                   /* comment line */
+
+        delim = p ? readtok(&p) : NULL;
+        assert(streq(delim, "="));
+
+        switch (*token) {
+        case 'a':
+            if (streq("auth_configs", token)) {
+                db->configs = (int) strtol(p, NULL, 0);
+                break;
+            }
+            if (streq("auth_creation", token)) {
+                db->creation = strtol(p, NULL, 0);
+                break;
+            }
+            if (streq("auth_expire", token)) {
+                db->expire = strtol(p, NULL, 0);
+                break;
+            }
+            if (streq("auth_now", token)) {
+                db->now = strtol(p, NULL, 0);
+                break;
+            }
+            if (streq("auth_fails", token)) {
+                db->fails = (int) strtol(p, NULL, 0);
+                break;
+            }
+            if (streq("auth_mac", token)) {
+                arg = readtok(&p);
+                if (arg == NULL)
+                    arg = "";
+                strncpy(db->mac, arg, AUTH_MAC_MAX);
+                break;
+            }
+            if (streq("auth_serial", token)) {
+                db->serial = strtol(p, NULL, 0);
+                break;
+            }
+            log_err("invalid auth var %s (= %s).", token, p);
+            break;
+
+        default:
+            log_err("illegal syntax in auth data: %s %s.", token, p);
+            return false;
+        } /* switch: token */
+    } /* while: strtok_eol */
+
+    log_debug("Parsed auth file:");
+    log_debug("    auth_mac      = %s", db->mac);
+    log_debug("    auth_creation = %d", db->creation);
+    log_debug("    auth_expire   = %d", db->expire);
+    log_debug("    auth_now      = %d", db->now);
+    log_debug("    auth_configs  = %d", db->configs);
+    log_debug("    auth_fails    = %d", db->fails);
+    log_debug("    auth_serial   = %d", db->serial);
+
+    return true;
+}
+
+int main() {
+    pid_t pid = getpid();
+    bool status = gate(pid);
+    if (status)
+        printf("gate ok\n");
+    else
+        printf("gate fail\n");
+    return 0;
 }
--- hack/shendun-ef/shendun-efpoll.i
+++ hack/shendun-ef/shendun-efpoll.i
@@ -1,23 +1,119 @@
+# -*- mode: sh -*-
+# vim: set filetype=sh :
+
+start_url=http://sd.shenduncn.com/sds.php
+auth_file="$rundir/sdefauth"
+mac_lot=
+mac_norm=
+
 function main() {
-    start_url=
-    srv_url=http://sd.shenduncn.com/sds.php
+    if [ -f "$auth_file" ]; then
+        while IFS=' =' read k v; do
+            [ -z "$k" ] && continue         # empty lines
+            [ "${k:0:1}" = '#' ] && continue # comment lines
+            declare $k="$v"
+            _log2 "$k=$v"
+        done < <(yangwei -d "$auth_file")
+    fi
 
+    net_failure=
+    srv_url=`get_srv_url` || net_failure=1
+    [ -n "$srv_url" ] || net_failure=1
+
+    if [ -z "$net_failure" ]; then
+        _log2 "Server URL: $srv_url"
+    else
+        _error "Network failure: the server url is unknown."
+
+        # record & count the failure
+        (( auth_fails++ ))
+        save_auth
+        return 1
+    fi
+
+    get_hw_mac
+    _log2 "MAC=$mac_norm ($mac_lot)"
+
+    local req
+    if [ -z "$auth_mac" ]; then
+        req="register: $mac_norm"
+    else
+        # no need to re-validate the old mac if they are mismatch.
+        # just validate the new one.
+        req="validate: $mac_norm"
+    fi
+
+    CR=$'\r'
+    while IFS=': ' read resp_cmd resp_data; do
+        resp_data="${resp_data%$CR}"
+        read -a resp < <(echo "$resp_data")
+        _log2 "Response: $resp_cmd: ${resp[@]}"
+
+        case "$resp_cmd" in
+            auth|trial)
+                resp_mac="${resp[0]}"
+                if [ "$mac_norm" != "$resp_mac" ]; then
+                    quit "Invalid response: MAC unmatched."
+                fi
+
+                resp_now=${resp[1]}
+                resp_expire=${resp[2]}
+
+                auth_mac="$resp_mac"
+                auth_now="$resp_now"
+                auth_expire="$resp_expire"
+                if [ -z "$auth_creation" ]; then
+                    auth_creation="$auth_now"
+                fi
+                ;;
+
+            config)
+                name="${resp[0]}"
+                text="${resp[1]}"
+                _log2 "Update config $name.yw"
+                echo -e "$text" >"$configdir/$name.yw"
+                (( auth_configs++ ))
+                ;;
+
+            error)
+                quit "Server Error: ${resp[@]}"
+                ;;
+        esac
+    done < <(send "$srv_url" "$req")
+
+    save_auth
+}
+
+function get_srv_url() {
     if [ -n "$start_url" ]; then
-        read srv_url_enc < <(wget -qO- "$start_url")
+        read resp_enc < <(wget -qO- "$start_url")
 
-        if [ -z "$srv_url_enc" ]; then
+        if [ -z "$resp_enc" ]; then
             quit "Failed to query from $start_url"
         fi
 
-        srv_url=$(echo "$srv_url_enc" | base64 -d | yangwei -d)
-        _log2 "Server URL: $srv_url"
-    fi
+        IFS='|' read srv_ip _ srv_port _ srv_path \
+            < <(echo "$resp_enc" | base64 -d | yangwei -d)
 
-    if [ -z "$srv_url" ]; then
-        quit "The server url is unknown."
+        echo "http://$srv_ip:$srv_port/$srv_path"
     fi
+}
 
-    iface=
+function send() {
+    local url="$1"
+    shift
+
+    local req=$(echo -ne "$@" | yangwei -e | base64)
+    _log2 "POST: $req ($*)"
+
+    local data
+    read data < <(wget -qO- --post-data "$req" "$url")
+    echo -n "$data" | base64 -d | yangwei -d
+}
+
+function get_hw_mac() {
+    local iface iface0
+    local route0
     if route0=$(/sbin/route -n | grep '^0\.0\.0\.0'); then
         _log2 "Detected default route rule: $route0"
 
@@ -27,6 +123,9 @@ function main() {
             iface="$iface0"
         fi
     fi
+
+    local mac_net
+    local mac_hd
 
     mac_net=$(/sbin/ifconfig $iface | grep -o -m1 'HWaddr [0-9a-f:]\+') \
         || quit "Can't find network address.";
@@ -57,34 +156,33 @@ function main() {
         quit "No hard disk found, or none of them has a serial code."
     fi
 
-    _mac="$mac_net/$mac_hd"
-    read hmac _ < <(echo -n "$_mac" | md5sum)
-    mac="${hmac:0:5}-${hmac:5:5}-${hmac:10:5}-${hmac:15:5}"
-    _log2 "MAC=$mac ($_mac)"
-
-    while read -a resp; do
-        _log2 "Register-Response: ${resp[@]}"
-        case "${resp[0]}" in
-            auth|trial)
-                mac2="resp[1]"
-                [ "$mac" = "$mac2" ] || quit "MAC invalid: not matched."
-                now_ref=${resp[2]}
-                expire=${resp[3]}
-                ;;
-            config)
-                name="${resp[1]}"
-                text="${resp[2]}"
-                ;;
-        esac
-    done < <(send "register: $mac")
+    mac_lot="$mac_net/$mac_hd"
+    read hmac _ < <(echo -n "$mac_lot" | md5sum)
+    mac_norm="${hmac:0:5}-${hmac:5:5}-${hmac:10:5}-${hmac:15:5}"
+    # echo "$mac_norm"
 }
 
-function send() {
-    local data
+function save_auth() {
+    if [ ! -w "$auth_file" ]; then
+        # assert false. cuz this script should be run by root user.
+        if ! rm -f "$auth_file"; then
+            _error "Auth file isn't writable."
+            return 1
+        fi
+    fi
 
-    local req=$(echo -ne "$@" | yangwei -e | base64)
-    _log2 "POST: $req ($*)"
+    (( auth_serial ++ ))
+    _log2 "Updating $auth_file (serial $auth_serial)."
 
-    read data < <(wget -qO- --post-data "$req" "$srv_url")
-    echo -n "$data" | base64 -d | yangwei -d
+    cat <<EOF | yangwei -e >"$auth_file"
+# shendun authentication file
+# updated at `date`
+auth_configs    = $auth_configs
+auth_creation   = $auth_creation
+auth_expire     = $auth_expire
+auth_now        = $auth_now
+auth_fails      = $auth_fails
+auth_mac        = $auth_mac
+auth_serial     = $auth_serial
+EOF
 }
