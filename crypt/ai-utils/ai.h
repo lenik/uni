72f13b005ef3533f37e20271600c2c806b9bab56
Lenik <pub.lenik@bodz.net>
2013-12-07 12:33:08 +0800

    ai-utils: Implemented encryption.

--- crypt/ai-utils/ai.h
+++ crypt/ai-utils/ai.h
@@ -12,20 +12,20 @@
  *   u32 size in bytes;
  */
 typedef struct _ai_header {
-    u_int16_t flags;
-    int8_t reserved;
+    int8_t flags;
     int8_t app;
+    u_int16_t checksum;
     u_int32_t size;
 } ai_header;
 
 #define AIH_SIZE sizeof(ai_header)
-#define AIF_ENCRYPT 1
+#define AI_ENCRYPT 1
 
 enum {
-    AIH_APP_NONE = 0,
-    AIH_APP_GZIP,
-    AIH_APP_BZIP2,
-    AIH_APP_XZ,
+    AI_APP_NONE = 0,
+    AI_APP_GZIP,
+    AI_APP_BZIP2,
+    AI_APP_XZ,
 };
 
 #endif
