e9786b857a09ded413e72159dd6cc4565f8b6ca8
Lenik <pub.lenik@bodz.net>
2013-12-25 13:49:11 +0800

    libbas-c: legacy fscanf: set to glibc 2.0 on i386 and 2.2.5 on amd64.

--- lib/libbas-c/include/bas/legacy/fscanf.h
+++ lib/libbas-c/include/bas/legacy/fscanf.h
@@ -1,7 +1,12 @@
 #ifndef __BAS_LEGACY_FSCANF
 #define __BAS_LEGACY_FSCANF
 
-__asm__(".symver fscanf_2_0,fscanf@GLIBC_2.0");
-#define fscanf fscanf_2_0
+#if __i386__
+    __asm__(".symver fscanf_2_0,fscanf@GLIBC_2.0");
+    #define fscanf fscanf_2_0
+#else
+    __asm__(".symver fscanf_2_2_5,fscanf@GLIBC_2.2.5");
+    #define fscanf fscanf_2_2_5
+#endif
 
 #endif
