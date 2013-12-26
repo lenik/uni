dccc958c4615af3e1985c833b2a93f735d0b5c51
Lenik <pub.lenik@bodz.net>
2013-12-26 14:05:15 +0800

    libbas-c: i18n: use PACKAGE_NAME as the default text domain.

--- lib/libbas-c/include/bas/i18n.h
+++ lib/libbas-c/include/bas/i18n.h
@@ -4,6 +4,16 @@
 #include <libintl.h>
 #include <locale.h>
 
-#define tr(s) gettext(s)
+#ifndef I18N_DOMAIN
+#   ifdef PACKAGE_NAME
+#       define I18N_DOMAIN PACKAGE_NAME
+#   endif
+#endif
+
+#ifdef I18N_DOMAIN
+#   define tr(s) dgettext(I18N_DOMAIN, s)
+#else
+#   define tr(s) gettext(s)
+#endif
 
 #endif
