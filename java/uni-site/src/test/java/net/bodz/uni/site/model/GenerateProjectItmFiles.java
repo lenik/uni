80d35e82b7b1e0a46cc8b3da036bc2697c8937cf
Lenik <pub.lenik@bodz.net>
2014-05-14 00:26:35 +0800

    Wrote the .itm files for all projects.

---
+++ java/uni-site/src/test/java/net/bodz/uni/site/model/GenerateProjectItmFiles.java
@@ -0,0 +1,48 @@
+package net.bodz.uni.site.model;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Assert;
+
+import net.bodz.bas.io.ITreeOut;
+import net.bodz.bas.io.res.builtin.FileResource;
+import net.bodz.bas.log.Logger;
+import net.bodz.bas.log.LoggerFactory;
+import net.bodz.uni.site.UniSiteServerConfig;
+
+public class GenerateProjectItmFiles
+        extends Assert {
+
+    static final Logger logger = LoggerFactory.getLogger(GenerateProjectItmFiles.class);
+
+    public static void main(String[] args)
+            throws IOException {
+        Site site = new Site(UniSiteServerConfig.workDir);
+        for (Section section : site.getSectionMap().values()) {
+            for (Project project : section.getProjects()) {
+                File docFile = new File(project.getDirectory(), project.getName() + ".itm");
+                FileResource file = new FileResource(docFile);
+                ITreeOut out = file.newTreeOut();
+                out.println("@label");
+                out.enter();
+                out.println(project.getName());
+                out.println("<p lang=\"zh-cn\">" + project.getName());
+                out.leave();
+                out.println();
+
+                out.println("@text");
+                out.enter();
+                String description = project.getDescription().toString();
+                out.println(description.replace("\n", "\n    "));
+                out.println();
+                out.println("<p lang=\"zh-cn\">");
+                out.println("没有描述。");
+                out.leave();
+                out.flush();
+                out.close();
+            }
+        }
+    }
+
+}
