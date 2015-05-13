0476c879fe3575ed9d9f6ee720bdca9fe12b51a5
Lenik <pub.lenik@bodz.net>
2015-05-13 13:19:45 +0800

    cocos2d-x-local: packaged.

---
+++ alt/cocos2d-x-local/cocos2d-x.sh
@@ -0,0 +1,17 @@
+
+# Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
+export COCOS_CONSOLE_ROOT=/opt/sdk/cocos2d-x/tools/cocos2d-console/bin
+
+# Add environment variable COCOS_TEMPLATES_ROOT for cocos2d-x
+export COCOS_TEMPLATES_ROOT=/opt/sdk/cocos2d-x/templates
+
+# Add environment variable ANT_ROOT for cocos2d-x
+export ANT_ROOT=/usr/share/ant/bin
+
+# Add environment variable ANDROID_SDK_ROOT for cocos2d-x
+export ANDROID_SDK_ROOT=/opt/sdk/android
+
+# Add environment variable NDK_ROOT for cocos2d-x
+export NDK_ROOT=/opt/sdk/android-ndk
+
+export PATH=$PATH:$COCOS_CONSOLE_ROOT:$NDK_ROOT
