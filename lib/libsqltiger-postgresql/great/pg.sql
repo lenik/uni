facb382b0ff09006568ce6c565bc96020f0ae5e6
Lenik <pub.lenik@bodz.net>
2015-12-06 22:22:43 +0800

    libsqltiger-postgresql: Integrated with catme libdirs.

---
+++ lib/libsqltiger-postgresql/great/pg.sql
@@ -0,0 +1,13 @@
+--\import great.pg.columnstat
+--\import great.pg.dropemptytables
+--\import great.pg.killtable
+--\import great.pg.mkaview
+--\import great.pg.monthtrends
+--\import great.pg.prevnext
+--\import great.pg.purgeschema
+--\import great.pg.renumschema
+--\import great.pg.renum
+--\import great.pg.seqmax
+--\import great.pg.sequsage
+--\import great.pg.tablestat
+--\import great.pg.valdist
