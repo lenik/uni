0683841d5e50337c4bf87f31128aab4a96351bc6
Lenik <pub.lenik@bodz.net>
2016-09-25 23:35:30 +0800

    autodir: Renamed to cwdex.

---
+++ util/cwdex/cwdex.sh
@@ -0,0 +1,63 @@
+
+function cwdex-cd() {
+    local type=$1
+    shift
+
+    local arg
+    local cdopts=()
+    local hasdir=0
+    local args=
+
+    for arg in "$@"; do
+        case "$arg" in
+            -*)
+                cdopts=("${cdopts[@]}" "$arg")
+                ;;
+            *)
+                if [ "$hasdir" = 0 ]; then
+                    cdopts=("${cdopts[@]}" "$arg")
+                    hasdir=1
+                else
+                    args=("${args[@]}" "$arg")
+                fi
+                ;;
+        esac
+    done
+
+    local olddir="$PWD"
+    builtin $type "${cdopts[@]}" || return
+    local newdir="$PWD"
+
+    if [ -f "$olddir/.cwdex/leave" ]; then
+        # leave() never fails.
+        cd "$olddir"
+        . ".cwdex/leave"
+        cd "$newdir"
+    fi
+
+    if [ -f ".cwdex/enter" ]; then
+        if ! . ".cwdex/enter"; then
+            echo "Failed to enter, return back to $olddir." >&2
+            case $type in
+                cd)
+                    builtin cd "$olddir";;
+                pushd)
+                    builtin popd;;
+                popd)
+                    builtin pushd "$olddir";;
+            esac
+            return 3
+        fi
+    fi
+
+    return 0
+}
+
+alias _cd='builtin cd'
+alias _pushd='builtin pushd'
+alias _popd='builtin popd'
+
+alias cd='cwdex-cd cd'
+alias chdir='cwdex-cd cd'
+alias pushd='cwdex-cd pushd'
+alias popd='cwdex-cd popd'
