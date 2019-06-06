696f1a8bbca4eabe969631f286b750439e14689d
Lenik <pub.lenik@bodz.net>
2019-06-06 21:49:33 +0800

    lilygen: mkv-andr refined custom-view support.

--- devel/lilygen/template/widget/widget.java
+++ devel/lilygen/template/widget/widget.java
@@ -3,26 +3,46 @@
     cat <<EOT >"$file"
 package $pkg;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 
 import $pkg_parent.R;
 
-public class ${Base}
-        extends RowLayout {
+public class $Base
+        extends LinearLayoutCompat {
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
+    public $Base(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        inflate(context, R.layout.$ba_se, this);
+
+        // TextView foobar = findViewById(R.id.foobar);
+        
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.$Base);
+        try {
+        } finally {
+            a.recycle();
         }
-        setContentView(R.layout.$ba_se);
     }
 
 }
 EOT
+
+    _log1 "Prepare styleable attributes for $fqcn"
+    attrs="app/src/main/res/values/attrs.xml"
+
+    tmp=`mktemp`
+    
+    grep -m1 -B1000000 '</resources>' "$attrs" | grep -v '</resources>' >$tmp
+    cat <<EOT >>$tmp
+
+    <declare-styleable name="$Base">
+    </declare-styleable>
+EOT
+
+    grep -m1 -A1000000 '</resources>' "$attrs" >>$tmp
+    
+    mv $tmp "$attrs"
 
