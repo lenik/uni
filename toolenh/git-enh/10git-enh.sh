0f05ffd3efcbaaaac5e9b592f4ea4a93127daa50
Lenik <pub.lenik@bodz.net>
2020-03-12 19:21:43 +0800

    git-enh: Removed complconfdir. Use g* exenames.

--- toolenh/git-enh/10git-enh.sh
+++ toolenh/git-enh/10git-enh.sh
@@ -1,5 +1,19 @@
 # vim: set filetype=sh :
 
+alias      gbr='git branch'
+alias      gci='git commit'
+alias   gclone='git clone'
+alias      gco='git checkout'
+alias      gdi='git diff'
+alias      gmt='git mergetool'
+alias     glog='git log'
+alias    gpull='git pull'
+alias     pure='git pull --rebase'
+alias    gpush='git push'
+alias   gstash='git stash'
+alias      gst='git status'
+alias     gtag='git tag'
+
 alias     cira='git commit --amend --reset-author -C HEAD'
 alias       gd='git delta'
 alias      gmt='git mergetool --tool=meld'
