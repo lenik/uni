93c7a678952964cd761c93235e69586a6898d671
Lenik <pub.lenik@bodz.net>
2021-09-13 10:18:03 +0800

    leitex: Replaced texlive-local with texlive-full.

--- docker/leitex/prebuild.sh
+++ docker/leitex/prebuild.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 
-    dir=tex
-
-    if [ ! -d "$dir/bin" ]; then
-        mkdir -vp $dir
-        echo mount -t auto -o bind /usr/local/tex $dir
-        sudo mount -t auto -o bind /usr/local/tex $dir
-    fi
-
