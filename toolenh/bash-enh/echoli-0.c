d8f7b6d561e4fff6dd3894ae0ab6c184fb5c10dd
Lenik <pub.lenik@bodz.net>
2023-03-03 19:09:09 +0800

    bash-enh: Add echoli-0.

---
+++ toolenh/bash-enh/echoli-0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    //argc--;
+    //argv++;
+
+    int i;
+    for (i = 0; i < argc; i++) {
+        printf("%d) %s.\n", i, argv[i]);
+    }
+
+    return 0;
+}
