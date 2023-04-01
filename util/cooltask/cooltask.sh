37efe750a2e98ff8ff6cc1fdbfb1b0ae71bda028
Lenik <pub.lenik@bodz.net>
2023-04-02 01:45:38 +0800

    cooltask: Add findcont and chtask.

--- util/cooltask/cooltask.sh
+++ util/cooltask/cooltask.sh
@@ -1,43 +1,4 @@
-. shlib-import findabc
-
-function findcont() {
-    local d="$1" # root dir
-    local n
-    local cd
-
-    shift
-    if [ $# = 0 ] || [ "$1" = '-l' ]; then
-        shift
-        cooltask_list "$@"
-        return
-    fi
-
-    if [ "$1" = '-C' ]; then cd=1; shift; fi
-    if [ "$1" = '-A' ]; then
-        shift
-        local abbr="$1"
-        local len=${#abbr} i
-        local opts=()
-        for (( i = 0; i < len; i++)); do
-            opts=("${opts[@]}" ${abbr:i:1})
-        done
-        set -- "${opts[@]}"
-    fi
-
-    while [ $# -ge 2 ]; do
-        if ! d=`findabc -ap "$1" "$d"`; then
-            echo "Failed to find $1 (in $d)" >&2
-            return 1
-        fi
-        shift
-    done
-
-    if [ "$cd" = 1 ]; then
-        findabc -a "$1/" "$d"
-    else
-        findabc -ap "$1" "$d"
-    fi
-}
+. shlib-import findcont
 
 # Using `T <job-class> <job-id>` for `J` and `Jwhich`.
 alias T='findcont %cooltask -C'
