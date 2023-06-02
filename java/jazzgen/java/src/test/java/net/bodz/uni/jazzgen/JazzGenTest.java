35acd131b503ebbd51e73e581a12204a984603d2
Lenik <pub.lenik@bodz.net>
2023-06-02 20:12:19 +0800

    jazzgen: Move ioforms from daogen to jazzgen. Add tcoll.

---
+++ java/jazzgen/java/src/test/java/net/bodz/uni/jazzgen/JazzGenTest.java
@@ -0,0 +1,14 @@
+package net.bodz.uni.jazzgen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JazzGenTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
