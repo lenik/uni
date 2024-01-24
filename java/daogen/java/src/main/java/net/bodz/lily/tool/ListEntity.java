21f59806641bb3f607ee7d1f7925a44b2c595232
Lenik <pub.lenik@bodz.net>
2024-01-24 19:10:36 +0800

    daogen: ListEntity: generate class-map for rst config.

--- java/daogen/java/src/main/java/net/bodz/lily/tool/ListEntity.java
+++ java/daogen/java/src/main/java/net/bodz/lily/tool/ListEntity.java
@@ -1,8 +1,6 @@
 package net.bodz.lily.tool;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import javax.persistence.Table;
 
@@ -13,11 +11,23 @@ import net.bodz.bas.c.type.GenericTypes;
 import net.bodz.bas.c.type.IndexedTypes;
 import net.bodz.bas.db.ibatis.IEntityMapper;
 import net.bodz.bas.db.ibatis.IMapper;
+import net.bodz.bas.io.ITreeOut;
+import net.bodz.bas.io.Stdio;
 import net.bodz.bas.meta.build.ProgramName;
 import net.bodz.bas.potato.PotatoTypes;
 import net.bodz.bas.potato.element.IType;
 import net.bodz.bas.program.skel.BasicCLI;
 import net.bodz.bas.t.order.AbstractNonNullComparator;
+import net.bodz.lily.model.base.CoCode;
+import net.bodz.lily.model.base.CoMomentInterval;
+import net.bodz.lily.model.base.CoNode;
+import net.bodz.lily.t.base.CoMessage;
+import net.bodz.lily.template.CoCategory;
+import net.bodz.lily.template.CoParameter;
+import net.bodz.lily.template.CoPhase;
+import net.bodz.lily.template.CoTag;
+import net.bodz.lily.template.CoTalk;
+import net.bodz.lily.template.VoteRecord;
 
 /**
  * List mapped entity types.
@@ -25,6 +35,13 @@ import net.bodz.bas.t.order.AbstractNonNullComparator;
 @ProgramName("ls-entity")
 public class ListEntity
         extends BasicCLI {
+
+    /**
+     * Select entity classes within these packages.
+     *
+     * @option -p =NAME
+     */
+    List<String> packageNames = new ArrayList<>();
 
     /**
      * Order by entity class name.
@@ -43,8 +60,12 @@ public class ListEntity
     @Override
     protected void mainImpl(String... args)
             throws Exception {
+        List<String> prefixes = new ArrayList<>();
+        for (String p : packageNames)
+            prefixes.add(p + ".");
+
         List<Item> list = new ArrayList<>();
-        for (Class<?> mapperClass : IndexedTypes.list(IMapper.class, true)) {
+        M: for (Class<?> mapperClass : IndexedTypes.list(IMapper.class, true)) {
             if (IEntityMapper.class.isAssignableFrom(mapperClass)) {
                 GenericActualClassInfo typeInfo = GenericTypes.getActualInfo(mapperClass);
                 if (typeInfo == null)
@@ -56,6 +77,11 @@ public class ListEntity
 
                 Class<?>[] bounds = info.getBounds();
                 Class<?> entityType = bounds[0];
+
+                for (String p : prefixes) {
+                    if (! entityType.getName().startsWith(p))
+                        continue M;
+                }
                 Item item = new Item(entityType);
                 list.add(item);
             }
@@ -68,8 +94,72 @@ public class ListEntity
             Collections.sort(list, new TableNameOrder());
         }
 
+        ITreeOut out = Stdio.cout.indented();
+
+        printClassMap(out, list);
+        printTableNames(out, list);
+
+    }
+
+    Set<Class<?>> roots = new LinkedHashSet<>();
+    {
+        roots.add(CoCategory.class);
+        roots.add(CoParameter.class);
+        roots.add(CoPhase.class);
+        roots.add(CoTag.class);
+        roots.add(CoCode.class);
+        roots.add(CoNode.class);
+
+        roots.add(CoTalk.class);
+        roots.add(CoMessage.class);
+        roots.add(CoMomentInterval.class);
+
+        roots.add(VoteRecord.class);
+        // roots.add(IdEntity.class);
+    }
+
+    void printClassMap(ITreeOut out, List<Item> list) {
+        Map<Class<?>, List<Item>> map = new LinkedHashMap<>();
         for (Item item : list) {
-            String name = item.name;
+            Class<?> javaClass = item.type.getJavaClass();
+            if (javaClass == null)
+                throw new NullPointerException("javaClass: " + item.type);
+            for (Class<?> root : roots) {
+                if (root.isAssignableFrom(javaClass)) {
+                    List<Item> children = map.get(root);
+                    if (children == null)
+                        map.put(root, children = new ArrayList<>());
+                    children.add(item);
+                }
+            }
+        }
+
+        out.println("class-map {");
+        out.enter();
+
+        for (Class<?> root : map.keySet()) {
+            List<Item> children = map.get(root);
+            out.println(root.getName() + ": \\");
+            out.enter();
+            int n = children.size();
+            for (int i = 0; i < n; i++) {
+                Item item = children.get(i);
+                out.print(item.tableName);
+                if (i != n - 1)
+                    out.print(" \\");
+                out.println();
+            }
+            out.leave();
+        }
+        out.leave();
+        out.println("}");
+    }
+
+    void printTableNames(ITreeOut out, List<Item> list) {
+        out.println("table-name {");
+        out.enter();
+        for (Item item : list) {
+            String name = item.tableName;
             if (name == null) {
                 name = item.type.getSimpleName();
                 name = name.toLowerCase();
@@ -78,6 +168,8 @@ public class ListEntity
                     name + ":", //
                     item.type.getJavaClass().getCanonicalName());
         }
+        out.leave();
+        out.println("}");
     }
 
     public static void main(String[] args)
@@ -86,14 +178,14 @@ public class ListEntity
     }
 
     static class Item {
-        String name;
+        String tableName;
         IType type;
 
         public Item(Class<?> clazz) {
             type = PotatoTypes.getInstance().getType(clazz);
             Table aTable = type.getAnnotation(Table.class);
             if (aTable != null)
-                name = aTable.name();
+                tableName = aTable.name();
         }
 
     }
@@ -105,7 +197,7 @@ public class ListEntity
 
         @Override
         public int compareNonNull(Item o1, Item o2) {
-            int cmp = Nullables.compare(o1.name, o2.name);
+            int cmp = Nullables.compare(o1.tableName, o2.tableName);
             if (cmp != 0)
                 return cmp;
             Class<?> c1 = o1.type.getJavaClass();
