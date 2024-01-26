50e990146496ec2be8c4d374e240eb86bd6f99f8
Lenik <pub.lenik@bodz.net>
2024-01-26 10:59:58 +0800

    bash-enh: refactor cd-first to cd-any which accepts file as arg.

--- toolenh/bash-enh/aliases.d/dirs.sh
+++ toolenh/bash-enh/aliases.d/dirs.sh
@@ -3,7 +3,7 @@ alias cd..='. cd..n'
 alias cd...='cd ../..'
 alias cd....='cd ../../..'
 alias ccd='. cd-ls'
-alias cdf='. cd-first'
+alias cda='cd-any'
 alias cd-alias='. cd-alias'
 
 alias md='mkdir'
@@ -18,3 +18,22 @@ alias du1='du --max-depth=1'
 alias du2='du --max-depth=2'
 
 alias treee='tree-less'
+
+function cd-any() {
+    local dir file
+
+	while read dir; do
+        cd "$dir"
+		return 0
+	done < <(find -path ./debian -prune -o -type d -path "*$1*" -print)
+
+	while read file; do
+        dir="${file%/*}"
+        cd "$dir"
+		return 0
+	done < <(find -path ./debian -prune -o -type f -path "*$1*" -print)
+
+    _error "can't find *$1*."
+    return 1
+}
+
