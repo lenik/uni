366991909de7fcc0873604eac57f588da346a53f
Lenik <pub.lenik@bodz.net>
2024-02-18 01:04:51 +0800

    skeljs: Add FieldGroup.

---
+++ lib/libjs-skeljs/packages/core/src/ui/FieldGroup.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { onMounted, ref } from "vue";
+import { } from "./types";
+
+export interface Props {
+}
+</script>
+
+<script setup lang="ts">
+const model = defineModel();
+
+const props = withDefaults(defineProps<Props>(), {
+});
+
+const emit = defineEmits<{
+    error: [message: string]
+}>();
+
+// property shortcuts
+
+const rootElement = ref<HTMLElement>();
+
+// methods
+
+defineExpose({ update });
+
+function update() {
+}
+
+onMounted(() => {
+});
+</script>
+
+<template>
+    <slot></slot>
+</template>
+
+<style scoped lang="scss">
+.field-group {
+    padding: 0;
+}
+</style>
