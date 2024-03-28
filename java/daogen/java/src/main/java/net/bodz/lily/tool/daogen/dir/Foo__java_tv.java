7f7fa5c93e78122854a9ca4e814d5da6772e61e1
Lenik <pub.lenik@bodz.net>
2024-03-28 13:02:01 +0800

    daogen: Foo.java: it's possible to not to reuse @PrimaryKey*.

--- java/daogen/java/src/main/java/net/bodz/lily/tool/daogen/dir/Foo__java_tv.java
+++ java/daogen/java/src/main/java/net/bodz/lily/tool/daogen/dir/Foo__java_tv.java
@@ -9,6 +9,7 @@ import net.bodz.bas.t.catalog.ITableMetadata;
 import net.bodz.bas.t.catalog.TableOid;
 import net.bodz.lily.entity.PrimaryKeyColumns;
 import net.bodz.lily.entity.PrimaryKeyProperties;
+import net.bodz.lily.tool.daogen.ColumnNaming;
 import net.bodz.lily.tool.daogen.JavaGenProject;
 import net.bodz.lily.tool.daogen.JavaGen__java;
 import net.bodz.lily.tool.daogen.util.TypeAnalyzer;
@@ -50,19 +51,23 @@ public class Foo__java_tv
 
         IColumnMetadata[] keyCols = table.getPrimaryKeyColumns();
         if (keyCols != null && keyCols.length != 0 && extend.javaBaseClass != null) {
+            String[] colNames = new String[keyCols.length];
+            for (int i = 0; i < keyCols.length; i++)
+                colNames[i] = keyCols[i].getName();
+
+            String[] colProps = new String[keyCols.length];
+            for (int i = 0; i < keyCols.length; i++) {
+                ColumnNaming cname = project.naming(keyCols[i]);
+                colProps[i] = cname.propertyName;
+            }
+
             PrimaryKeyColumns aColumns = extend.javaBaseClass.getAnnotation(PrimaryKeyColumns.class);
-            if (aColumns == null) {
-                String[] colNames = new String[keyCols.length];
-                for (int i = 0; i < keyCols.length; i++)
-                    colNames[i] = keyCols[i].getName();
+            if (aColumns == null || ! arrayEquals(aColumns.value(), colNames)) {
                 annotateStrings(out, PrimaryKeyColumns.class, colNames);
             }
 
             PrimaryKeyProperties aProperties = extend.javaBaseClass.getAnnotation(PrimaryKeyProperties.class);
-            if (aProperties == null) {
-                String[] colProps = new String[keyCols.length];
-                for (int i = 0; i < keyCols.length; i++)
-                    colProps[i] = keyCols[i].getProperty().getName();
+            if (aProperties == null || ! arrayEquals(aProperties.value(), colProps)) {
                 annotateStrings(out, PrimaryKeyProperties.class, colProps);
             }
         }
@@ -93,6 +98,15 @@ public class Foo__java_tv
         }
         out.println();
         out.println("}");
+    }
+
+    boolean arrayEquals(String[] a, String[] b) {
+        if (a.length != b.length)
+            return false;
+        for (int i = 0; i < a.length; i++)
+            if (! a[i].equals(b[i]))
+                return false;
+        return true;
     }
 
 }
