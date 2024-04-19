15b88ba8d204d69904c86bf3facbadf1be682f27
Lenik <pub.lenik@bodz.net>
2024-04-19 08:01:28 +0800

    skeljs: Add FontAwesomeGallery and metadata json.

---
+++ lib/libjs-skeljs/packages/core/test/demo/FontAwesomeGalleryDemo.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+const title = "component FontAwesomeGalleryDemo";
+
+import { onMounted, ref } from "vue";
+
+export interface Props {
+}
+</script>
+
+<script setup lang="ts">
+import Icon from "skeljs/core/src/ui/Icon";
+import FontAwesomeGallery from "./FontAwesomeGallery";
+
+const model = defineModel();
+
+const props = withDefaults(defineProps<Props>(), {
+});
+
+const emit = defineEmits<{
+    error: [message: string]
+}>();
+
+// property shortcuts
+
+// app state
+
+// DOM references
+const rootElement = ref<HTMLElement>();
+const fontAwesomeGalleryElement = ref<InstanceType<typeof FontAwesomeGallery>>();
+
+// methods
+defineExpose({ update });
+
+function update() {
+}
+
+onMounted(() => {
+});
+</script>
+
+<template>
+    <div class="component-root" ref="rootElement">
+        <FontAwesomeGallery ref="fontAwesomeGalleryElement">
+        </FontAwesomeGallery>
+        <div @click="emit('error', 'this is a test.')">
+            Emit an error.
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.component-root {
+    padding: 0;
+}
+</style>
