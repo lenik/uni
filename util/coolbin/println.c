b550bb97b57c3988d99fb59d179e45ae90de616f
Lenik <pub.lenik@bodz.net>
2025-05-12 19:28:07 +0800

    coolbin: Renamed echo-x to echo-exec, add echo-n, println.

---
+++ util/coolbin/println.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    int i;
+    char *file;
+
+    argv++; argc--;
+    file = argv[0];
+    for (i = 0; i < argc; i++) {
+        if (i > 0)
+            putchar(' ');
+        printf("%s", argv[i]);
+    }
+    putchar('\n');
+    return 0;
+}
